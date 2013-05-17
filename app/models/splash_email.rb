48bcd48c54afa26061c220761147e9972f199549
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-05-17 15:40:02 -0700

    write unit tests for splash page

--- app/models/splash_email.rb
+++ app/models/splash_email.rb
@@ -2,6 +2,11 @@ class SplashEmail < ActiveRecord::Base
   attr_accessible :email
   
   validates_presence_of :email
-  #validates_uniqueness_of :email
+  validates_uniqueness_of :email
 
+  after_create :send_signup_confirm
+
+  def send_signup_confirm
+    SplashMailer.splash_signup_confirmation_email(self).deliver!
+  end
 end
