d17a40d77ec65b4752d108cbd8b8e92f5135660d
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-05-18 22:10:04 -0700

    add specs for thank_you page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -24,7 +24,49 @@ describe "StaticPages" do
       it "should create the splash_email instance" do
         expect { click_button("commit") }.to change(SplashEmail, :count).by(1)
       end
+
+      describe "after form submission" do
+        before(:each) do
+          click_button("commit")
+        end
+
+        it "should redirect to the thank_you page" do
+          subject.should have_selector('h2', text: "Thank you!")
+        end
+      end
     end
+
+  end
+
+  describe "Thank You Page" do
+
+    before { visit thank_you_path }
+    let(:suggestion_body){"testing123 123"}
+
+    it { should have_selector('h2', text: "Thank you!") }
+
+    describe "filling in the suggestion form" do
+      before(:each) do
+        ActionMailer::Base.deliveries = []  # Clear out other test deliveries
+        # save_and_open_page
+        fill_in("suggestion", with: suggestion_body)
+      end
+
+      context "after submitting the form" do
+        before(:each) do
+          click_button("commit")
+        end
+
+        it "should disable the form" do
+          # fail.  Can't test AJAX/JS behavior easily...
+        end
+        it "should send an email request with the form contents" do
+          ActionMailer::Base.deliveries.first.encoded.should include suggestion_body
+        end
+      end
+
+    end
+
 
   end
 
