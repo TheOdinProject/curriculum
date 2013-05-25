75304ead44244c3febd70f76fc902d07ba74a7c4
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-05-24 21:25:57 -0700

    authenticate requests, change from 'title' to 'summary' for calEvents in the JS

--- app/controllers/cal_events_controller.rb
+++ app/controllers/cal_events_controller.rb
@@ -1,38 +1,50 @@
 class CalEventsController < ApplicationController
+  # Note that FullCalendar uses Title in place
+  # of where I am using Summary
+  before_filter :authenticate_request
 
-  # this endpoint should return all events that
+  # this AJAX endpoint should return all events that
   # correspond to the calendar's filter settings
   def index
-    events = [
+    events = CalEvent.all.map do |event|
       {
-        id: 123,
-        title: "test 1",
+        id: event.id,
+        title: event.summary,
         allDay: false,
-        start: Time.now,
-        end: Time.now + 3600,
+        start: event.start,
+        end: event.end,
         url: "http://www.google.com",
         editable: true,
-      },
-      {
-        id: 124,
-        title: "test 2",
-        start: Time.now + 7200
       }
-    ]
+    end
+    puts "\n\n\n toJSON: #{events.to_json}! \n\n\n"
     render :json => events
   end
 
 
-  # this endpoint should create a new calendar
+  # this AJAX endpoint should create a new calendar
   # event and return it
   def create
     puts "\n\n\n #{params} \n\n\n"
-    event = {
-      title: params[:title],
-      start: params[:start],
-      end: params[:end]
-    }
-    render :json => event
+
+    event = CalEvent.new({
+      :summary => params[:summary],
+      :start => params[:start],
+      :end => params[:end],
+      :creator_id => current_user.id 
+    })
+    if event.save
+      render :json => event, :status => 201
+    else
+      render :json => event, :status => 400
+    end
   end
+
+  private
+    def authenticate_request
+      unless user_signed_in?
+        render :nothing => true, :status => 401 
+      end
+    end
 
 end
