57b2760ea806ee13d5c42d57990daffd9c98e4c1
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-06-03 01:30:26 -0700

    remove asset precompile false

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,7 @@ end
 module Theodinproject
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
 
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
