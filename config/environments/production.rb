88981efedac3159f5037829242c4bf6949d62d8a
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-06-03 01:33:02 -0700

    asset compile true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Theodinproject::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #false
 
   # Generate digests for assets URLs
   config.assets.digest = true
