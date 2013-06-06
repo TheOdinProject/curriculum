b34f844ef8daa714279ba82b318240aaee245288
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-06-05 19:16:28 -0700

    Add username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates_uniqueness_of :email, :username
+  validates :username, :length => { :in => 4..20 }
 
   has_many :cal_events, :foreign_key => :creator_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130528232335) do
+ActiveRecord::Schema.define(:version => 20130606015455) do
 
   create_table "cal_events", :force => true do |t|
     t.string   "summary"
@@ -49,9 +49,11 @@ ActiveRecord::Schema.define(:version => 20130528232335) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
