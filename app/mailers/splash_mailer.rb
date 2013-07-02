8198559356e8eedfdb04403905dbeacb95b3ad1e
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-07-02 12:45:13 -0700

    update splash mailer copy to include curriculum link

--- app/mailers/splash_mailer.rb
+++ app/mailers/splash_mailer.rb
@@ -3,10 +3,12 @@ class SplashMailer < ActionMailer::Base
   FEEDBACK_EMAIL = "feedback@theodinproject.com"
   CONTACT_EMAIL = "contact@theodinproject.com"
   ODIN_URL = "http://www.theodinproject.com"
+  CURRICULUM_URL = "http://www.github.com/TheOdinProject/curriculum"
 
   def splash_signup_confirmation_email(splash_email)
     @feedback_email = FEEDBACK_EMAIL
     @odin_url = ODIN_URL
+    @curriculum_url = CURRICULUM_URL
     mail( { :to => splash_email.email, 
             :subject => "Thank You!",
             :bcc => "Erik Trautman <erik@theodinproject.com>"} )
