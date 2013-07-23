2b814f2df213bbe8ea669b748f23b9bb6bb5b153
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-07-23 17:31:25 -0400

    project clarification

--- test.rb
+++ test.rb
@@ -1,10 +1,37 @@
-inputs = ARGV
-inputs.each_with_index do |input, i|
-  puts "Input at #{i} is #{input}!"
+# inputs = ARGV
+# inputs.each_with_index do |input, i|
+#   puts "Input at #{i} is #{input}!"
+# end
+# puts "ARGV splatted is: #{ARGV.inspect}"
+# puts "FILE is #{__FILE__}!"
+# puts "$0 is #{$0}!"
+# puts "$: is #{$:}!"
+# puts "$LOAD_PATH is #{$LOAD_PATH}!"
+# puts "I'm a command line script now!" if $0 == __FILE__
+
+module My_enum
+
+  def printer
+    puts "PRINTER"
+  end
+
 end
-puts "ARGV splatted is: #{ARGV.inspect}"
-puts "FILE is #{__FILE__}!"
-puts "$0 is #{$0}!"
-puts "$: is #{$:}!"
-puts "$LOAD_PATH is #{$LOAD_PATH}!"
-puts "I'm a command line script now!" if $0 == __FILE__
+
+class Array
+  include My_enum
+
+  def self.boom
+    puts "BOOM! CLASS METHOD!"
+  end
+
+  def random
+    puts "RANDOM"
+  end
+
+
+end
+
+
+[].random
+Array.boom
+[].printer
