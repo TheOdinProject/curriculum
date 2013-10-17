4786a5857069ac2f2c27e61f335b1aa8f4db7de1
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-17 16:30:36 -0700

    fix validations for lesson.course

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,6 @@ class Lesson < ActiveRecord::Base
   attr_accessible :course_id, :description, :is_project, :position, :section_id, :title, :url
 
   belongs_to :section
-  belongs_to :course, :through => :section
+  has_one :course, :through => :section
 
 end
