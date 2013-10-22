c268f216821562a2f3fb1f7322d6233b9c3df137
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-21 18:49:51 -0700

    fix brief_desc column from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019000347) do
+ActiveRecord::Schema.define(:version => 20131022014545) do
 
   create_table "cal_events", :force => true do |t|
     t.string   "summary"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(:version => 20131019000347) do
     t.string   "you_learn"
     t.string   "you_build"
     t.string   "teaser"
-    t.string   "brief_desc"
+    t.text     "brief_desc"
   end
 
   add_index "courses", ["title"], :name => "index_courses_on_title", :unique => true
