47697298bf186927d3d52a43ba517c8a9e90efbf
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-11-07 10:03:26 -0800

    fix forum spec div id

--- spec/features/forums_spec.rb
+++ spec/features/forums_spec.rb
@@ -8,7 +8,9 @@ describe "Forums" do
 
     before { visit forum_path }
 
-    it { should have_css('div#forum') }
+    it { 
+      # save_and_open_page
+      should have_css('div#moot-forum') }
 
     # We will need to build some tests to verify that the forums load properly
 
