c5da51106db93ee188ff915acae01b6896e48cd6
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-11-07 17:16:59 -0800

    courses controller specs

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
 
   # The top level page that lists all courses, e.g. Web Development 101, Ruby, Ruby on Rails etc...
   def index
-  @courses = Course.order("position asc")
+    @courses = Course.order("position asc")
   end
 
 end
