f02fa277100a8c059890c74f1aa9d254b23b2b76
Joseph Sawyer <zkay@mail.com>
2014-01-05 12:14:56 -0600

    fix production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Theodinproject::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
