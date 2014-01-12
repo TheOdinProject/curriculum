f74da99bdaeb2a818f5895dbdda28aeb157e3384
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-12 03:37:24 +0000

    AdminFlash integration test

--- spec/features/admin_flash_spec.rb
+++ spec/features/admin_flash_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe "Admin Flash" do
+  before { AdminFlash.create(message: "Some Message", expires: 2.days.from_now) }
   
-  # subject {page}
-  
-  context "on the courses page" do  
-    # shorter? before { visit help_path }
-    # will let work for creating a message?
-    # what selector will it have? it { should have_content('$selector') }
-    before do
-      AdminFlash.create(message: "Some Message", expires: 2.days.from_now)
-      visit courses_path
+  context "on the courses page" do    
+    before { visit courses_path }
+    it { expect(page).to have_content("Some Message") }
+    
+    context "shouldn't display expired message(s)" do
+      before do 
+        @new_admin_flash = AdminFlash.create(message: "Expired message", expires: 1.day.ago)
+        visit courses_path
+      end
+    
+      it { expect(page).to_not have_content("Expired message") }
     end
-    
-    subject { page }
-    
-    it "should have AdminFlash message" do
-      expect(subject).to have_content("Some Message")
-    end
-    
   end   
+
+ 
+  
     
 end  
