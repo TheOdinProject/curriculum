7f0300b33d4a96c624028993ca9be69653775c8d
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-14 05:01:51 +0000

    refactor signin portion of tests to a helper method

--- spec/features/admin_flash_spec.rb
+++ spec/features/admin_flash_spec.rb
@@ -24,10 +24,7 @@ describe "Admin Flash" do
     
     let(:user) { FactoryGirl.create(:user) }
     before do
-      visit login_path
-      fill_in "Email", :with => user.email
-      fill_in "Password", :with => user.password
-      click_button "Sign in"
+      sign_in_user(user)
       visit courses_path
     end
     it "should display messages" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'helpers/user_helper.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
