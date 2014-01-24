0914b70791c40207cf67987975f7b9829757a061
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-24 00:05:09 +0000

    remove comments

--- app/models/admin_flash.rb
+++ app/models/admin_flash.rb
@@ -3,11 +3,6 @@ class AdminFlash < ActiveRecord::Base
   
   validates :message, presence: true        
   validates :expires, presence: true  
- 
-#  If we were to use validates_timeliness gem :   
-#  validates_datetime :start_date,
-#    :after => :now,
-#    :after_message => "Event cannot start in the past"
 
   def self.unexpired_messages
     self.order("created_at desc").where("expires >= ?", Time.now)
