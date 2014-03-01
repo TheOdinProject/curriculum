f320e21424b862e3e5b1da6da3c6c600576029cc
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-03-01 15:33:41 -0800

    compress JS and CSS files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ Theodinproject::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
+  config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
