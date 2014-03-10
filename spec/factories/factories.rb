bcbedfa2af7d8e31e35a9447dba47d2e67f9a608
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-03-09 23:09:31 -0700

    remove all scheduler-related specs

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -22,17 +22,6 @@ FactoryGirl.define do
     
   end
 
-  factory :cal_event do
-    summary "Title"
-    start "#{DateTime.now()}"
-    self.end "#{DateTime.now()+3600}"
-    association :creator
-  end
-
-  factory :invalid_cal_event, :parent => :cal_event do
-    summary ""
-  end
-
   factory :content_bucket do
     sequence :name do |n| 
       "foobarContent#{n}"
