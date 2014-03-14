9137700c91551831d1772be4ba0db126676e9c5e
mach1010 <jetstream1010@gmail.com>
2014-03-13 20:33:43 -0500

    fixed for contributing PR

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140310055320) do
 
   # These are extensions that must be enabled in order to support this database
@@ -115,8 +114,8 @@ ActiveRecord::Schema.define(version: 20140310055320) do
   add_index "user_prefs", ["user_id"], name: "index_user_prefs_on_user_id", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -125,8 +124,8 @@ ActiveRecord::Schema.define(version: 20140310055320) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "username"
     t.text     "about"
     t.string   "github"
@@ -136,12 +135,9 @@ ActiveRecord::Schema.define(version: 20140310055320) do
     t.string   "google_plus"
     t.string   "skype"
     t.string   "screenhero"
-    t.boolean  "legal_agreement",        default: false, null: false
-    t.datetime "legal_agree_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["legal_agreement"], name: "index_users_on_legal_agreement", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
