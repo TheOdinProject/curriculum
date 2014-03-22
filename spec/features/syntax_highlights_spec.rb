124fa4c8b8365491c6a6d8939518c58b48b8ddfe
Afshin Mokhtari <afshinator@hotmail.com>
2014-03-22 19:24:46 +0000

    update syntax_highlights_spec.rb with the real updated code

--- spec/features/syntax_highlights_spec.rb
+++ spec/features/syntax_highlights_spec.rb
@@ -4,22 +4,16 @@ describe "Syntax-highlights" do
   subject { page } 
   context "on a lesson with properly fenced and identified ruby code" do
     
-    # Create a lesson to be displayed
-    # Fill that lesson with proper fenced code identification
-    # Test for classes via JS
     let!(:course) { FactoryGirl.create(:course)}
-    let!(:section) { FactoryGirl.create(:section)}
-    let!(:lesson) { FactoryGirl.create(:lesson, :content => "```lang-ruby\n I am ruby code```")}
+    let!(:section) { FactoryGirl.create(:section, :course_id => course.id)}
+    let!(:lesson) { FactoryGirl.create(:lesson, :section_id => section.id, :content => "```lang-ruby\n # I am a ruby comment\nputs \"Hi and stuff\"\n```")}
     
-    # pending "- Prism.js classes seem to be added after capybara runs test!"
+    before do
+      visit lesson_path(course.title_url, lesson.title_url)
+    end
     
-    # Change next line to go to a page with code and marked up with ```language
-    before { visit lesson_path(course.title_url, lesson.title_url) }
-    
-    # Checks for 'token' class on span, which gets added by Prism.js,
-    # doesn't check for 'class.lang-xxx' because that's Red Carpets job and lang specific.
-    it "should have the proper prism.js ruby class" do :js => true
-      expect(page).to have_css('span.token')
+    it "should have the proper prism.js ruby class", :js => true do
+      expect(page).to have_css('.language-ruby')
     end
   end
 end
