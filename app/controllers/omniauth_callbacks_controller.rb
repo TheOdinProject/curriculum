c73ec05439d06c1b629a68c298791816a76430bd
Nathan Hall <nathan.mh@gmail.com>
2014-04-01 10:21:23 -0500

    turbolinks added

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -18,13 +18,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.create(provider: omniauth['provider'], uid: omniauth['uid'], email: omniauth['info']['email'], username: omniauth['info']['nickname'])
     # session[:omniauth] = omniauth.except('extra')
     if user
-      flash[:notice] = "Signed in successfully"
+      flash.now[:notice] = "Signed in successfully"
       # current_user = user
-      sign_in(:user, user)
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      binding.pry
-      # sign_in_and_redirect(:user, user)
-      redirect_to courses_url
+      #sign_in(:user, user)
+      #@current_user ||= User.find(session[:user_id]) if session[:user_id]
+      sign_in_and_redirect(:user, user)
+      #redirect_to courses_url
     else
       session[:omniauth] = omniauth.except('extra')
       redirect_to new_user_registration_url
