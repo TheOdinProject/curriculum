99ed4677a38c8e55a48a696613aaab83355e8d1e
Ian White <ianwhite@utexas.edu>
2014-04-10 11:17:27 -0500

    Fix omniauth controller logic

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,11 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if User.find_by_email(request.env["omniauth.auth"]["info"]["email"])
-      user.provider = request.env["omniauth.auth"]["provider"]
-      user.uid = request.env["omniauth.auth"]["uid"]
-      sign_in_and_redirect user
-    elsif user.persisted?
+    if user.persisted?
       flash.notice = "Signed in!"
       sign_in_and_redirect user
     else
