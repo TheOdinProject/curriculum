5e3749ab75509f93c2dc22f6bdb3d2a9ada4d381
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-04-24 21:24:54 -0700

    add simple specs for ads

--- spec/factories/ads.rb
+++
@@ -1,6 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :ad do
-  end
-end
--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Ad do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "with one ad created" do
+  
+    let(:ad){ FactoryGirl.create(:ad) }
+
+    describe ".show_ve_banner?" do
+
+      context "with recently created user signed in" do
+        it "should show the ad" do
+          current_user = double("User", :created_at => 1.day.ago)
+          expect(Ad.show_ve_banner?(current_user)).to eq(true)
+        end
+      end
+
+      context "with longtime user signed in" do
+        it "should NOT show the ad" do
+          current_user = double("User", :created_at => 11.days.ago)
+          expect(Ad.show_ve_banner?(current_user)).to eq(false)
+        end
+      end
+
+      context "with anonymous visitor" do
+        it "should show the ad" do
+          expect(Ad.show_ve_banner?(nil)).to eq(true)
+        end
+      end
+    end
+  end
 end
