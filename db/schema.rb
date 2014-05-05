7e228b609c25855fed5415dfa88ecbac374a2c73
Abigail Jones <AbbyJonesDev@gmail.com>
2014-05-04 20:40:36 -0500

    Fill in migration for adding confirmable.  Because the blank one wasn't doing much...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505004746) do
+ActiveRecord::Schema.define(version: 20140505013220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,8 +139,14 @@ ActiveRecord::Schema.define(version: 20140505004746) do
     t.datetime "legal_agree_date"
     t.string   "provider"
     t.string   "uid"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.boolean  "reg_before_conf",        default: false
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["legal_agreement"], name: "index_users_on_legal_agreement", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
