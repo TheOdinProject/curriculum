fd2d7cbfbff3e03b911d5a4155dda3fe8376952c
Mazin Power <mazin.power@icloud.com>
2016-02-21 11:46:02 +0000

    Configured assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets do |config|
+  config.precompile += %w( twitter/bootstrap/glyphicons-halflings.png )
+  config.precompile += %w( twitter/bootstrap/glyphicons-halflings-white.png )
+  config.precompile += %w( fontawesome-webfont.eot )
+  config.precompile += %w( fontawesome-webfont.woff )
+  config.precompile += %w( fontawesome-webfont.ttf )
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 DEFAULT_HOST = "localhost"
