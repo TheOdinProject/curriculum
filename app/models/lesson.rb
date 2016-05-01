dc46fbf091f3fb3ba87e057da89de93c1098513d
Kevin <kevinmulhern23@gmail.com>
2016-05-01 23:18:58 +0100

    refactored next and previous lesson methods

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,21 +8,36 @@ class Lesson < ActiveRecord::Base
   validates_uniqueness_of :position
 
   def next_lesson
-    lessons = self.course.lessons.order("position asc")
-    if self.position >= lessons.first.position + lessons.size - 1
-      return nil
-    else
-      return lessons.find_by_position(self.position + 1)
-    end
+    find_lesson(next_lesson_position) if !last_lesson?
   end
 
   def prev_lesson
-    lessons = self.course.lessons.order("position asc")
-    if self.position <= 1
-      return nil
-    else
-      return lessons.find_by_position(self.position - 1)
-    end
+    find_lesson(previous_lesson_position) if !first_lesson?
   end
 
+  private
+
+  def first_lesson?
+    self.position <= 1
+  end
+
+  def lessons
+    self.course.lessons.order("position asc")
+  end
+
+  def last_lesson?
+    self.position >= lessons.first.position + lessons.size - 1
+  end
+
+  def find_lesson(lesson)
+    lessons.find_by_position(lesson)
+  end
+
+  def next_lesson_position
+    self.position + 1
+  end
+
+  def previous_lesson_position
+    self.position - 1
+  end
 end
