67909eb1965cdfe9e82608c42733dd940ae09ce8
Kevin <kevinmulhern23@gmail.com>
2016-05-08 19:52:51 +0100

    refactored find lesson behaviour into service object

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,36 +8,16 @@ class Lesson < ActiveRecord::Base
   validates_uniqueness_of :position
 
   def next_lesson
-    find_lesson(next_lesson_position) if !last_lesson?
+    find_lesson.next_lesson
   end
 
   def prev_lesson
-    find_lesson(previous_lesson_position) if !first_lesson?
+    find_lesson.prev_lesson
   end
 
   private
 
-  def first_lesson?
-    self.position <= 1
-  end
-
-  def lessons
-    self.course.lessons.order("position asc")
-  end
-
-  def last_lesson?
-    self.position >= lessons.first.position + lessons.size - 1
-  end
-
-  def find_lesson(lesson)
-    lessons.find_by_position(lesson)
-  end
-
-  def next_lesson_position
-    self.position + 1
-  end
-
-  def previous_lesson_position
-    self.position - 1
+  def find_lesson
+    FindLesson.new(self)
   end
 end
---
+++ app/services/find_lesson.rb
@@ -0,0 +1,42 @@
+class FindLesson
+
+  attr_reader :lesson
+
+  def initialize(lesson)
+    @lesson = lesson
+  end
+
+  def next_lesson
+    find_lesson(next_lesson_position) unless last_lesson?
+  end
+
+  def prev_lesson
+    find_lesson(previous_lesson_position) unless first_lesson?
+  end
+
+  private
+
+  def first_lesson?
+    lesson.position <= 1
+  end
+
+  def lessons
+    lesson.course.lessons.order("position asc")
+  end
+
+  def last_lesson?
+    lesson.position >= lessons.first.position + lessons.size - 1
+  end
+
+  def find_lesson(lesson)
+    lessons.find_by_position(lesson)
+  end
+
+  def next_lesson_position
+    lesson.position + 1
+  end
+
+  def previous_lesson_position
+    lesson.position - 1
+  end
+end
