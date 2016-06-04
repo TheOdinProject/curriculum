5345e493cf5c8e6b993aed65244b8b80f6cffa79
Kevin <kevinmulhern23@gmail.com>
2016-06-04 17:21:20 +0100

    added rails helper and tests for lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,31 +2,24 @@ class LessonsController < ApplicationController
 
   def index
     @course = find_course
-
-    if @course.nil?
-      not_found_error
-    end
   end
 
   def show
     @course = find_course
     @lesson = find_lesson
 
-    if @course.nil? || @lesson.nil?
-      not_found_error
-    end
-
     if show_ads?
       @lower_banner_ad = true
       @right_box_ad = true
     end
   end
-
-  private
-
   def show_ads?
     ENV["SHOW_ADS"] && Ad.show_ads?(current_user)
   end
+
+  private
+
+
 
   def find_course
     @find_course ||= Course.find_by_title_url!(params[:course_name])
@@ -34,9 +27,5 @@ class LessonsController < ApplicationController
 
   def find_lesson
     @find_lesson ||= find_course.lessons.find_by_title_url!(params[:lesson_name])
-  end
-
-  def not_found_error
-    raise ActionController::RoutingError.new('Not Found')
   end
 end
