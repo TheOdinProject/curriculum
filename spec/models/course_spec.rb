1d60a6afb0fddfb0d7ed876137b62f00d0eb30ed
Kevin <kevinmulhern23@gmail.com>
2016-10-03 15:41:12 +0100

    removed feature tests for lesson index page and cleaned up some tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Course do
   subject(:course) { Course.new(position: 1) }
+
   let(:sections) { [section1, section2] }
   let(:section1) { double('Section', position: 1, lessons: lessons) }
   let(:section2) { double('Section', position: 2, lessons: lessons) }
@@ -28,36 +29,37 @@ describe Course do
       allow(current_user).to receive(:completed_lessons).and_return([])
     end
 
-    context "for a user who completed no lessons" do
-      it "should return 0" do
+    context "when a user has completed no lessons" do
+      it "returns 0" do
         expect(course.percent_completed_by(current_user)).to eq(0)
       end
     end
 
-    context "for a user who completed the only lesson" do
+    context "when a user has completed the only lesson" do
 
       before do
         allow(course).to receive(:lessons).and_return([lesson])
         allow(current_user).to receive(:completed_lessons).and_return([lesson])
       end
 
-      it "should return 100" do
+      it "returns 100" do
         expect(course.percent_completed_by(current_user)).to eq(100)
       end
     end
 
-    context "for a user who completed one of four lessons" do
+    context "when a user has completed one of four lessons" do
       let(:lesson1) { double("Lesson") }
       let(:lesson2) { double("Lesson") }
       let(:lesson3) { double("Lesson") }
       let(:lesson4) { double("Lesson") }
       let(:lessons) { [lesson1, lesson2, lesson3, lesson4] }
+
       before do
         allow(course).to receive(:lessons).and_return(lessons)
         allow(current_user).to receive(:completed_lessons).and_return([lesson1])
       end
 
-      it "should return 25" do
+      it "returns 25" do
         expect(course.percent_completed_by(current_user)).to eq(25)
       end
     end
@@ -69,8 +71,9 @@ describe Course do
     before do
       allow(sections).to receive(:order).with('position asc').
         and_return(sections)
-        allow(sections).to receive(:includes).with(:lessons).
-          and_return(sectionsAsc)
+
+      allow(sections).to receive(:includes).with(:lessons).
+        and_return(sectionsAsc)
     end
 
     it 'returns sections in course' do
