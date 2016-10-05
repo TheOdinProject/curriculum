26476324cc38cccf729d1e0b8eb8358a1296ab01
Kevin <kevinmulhern23@gmail.com>
2016-10-05 20:11:30 +0100

    Refactored users helper and added unit tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,48 +1,32 @@
 module UsersHelper
+
   def gravatar_url(user, size)
-    gravatar_hash = Digest::MD5.hexdigest(user.email.strip.downcase)
-    default_url = "http://www.gravatar.com/avatar/436053b3e050d4156773bc04cfb167fe?s=#{size}"
-    # #{root_url}assets/odin_head_silhouette_2_pale.gif
-    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_hash}?s=#{size}&d=#{default_url}"
+    GravatarUrlBuilder.new(user, size).url
   end
 
   def twitter_url(user)
-    url = ""
-    if user.twitter
-      if user.twitter[0] == "@"
-        url = "http://www.twitter.com/#{user.twitter[1..-1]}"
-      else
-        url = "http://www.twitter.com/#{user.twitter}"
-      end
-    end
-  end 
+    social_media_link.new(user).twitter_url
+  end
 
   def github_url(user)
-    url = ""
-    if user.github
-      if user.github[0..3] == "http"
-        url = user.github
-      else
-        url = "http://www.github.com/#{user.github}"
-      end
-    end
-
+    social_media_link.new(user).github_url
   end
 
   def facebook_url(user)
-    url = ""
-    if user.facebook
-      url = "https://www.facebook.com/#{user.facebook}"
-    end
+    social_media_link.new(user).facebook_url
   end
+
   def linkedin_url(user)
-    url = ""
-    if user.linkedin
-      url = "https://www.linkedin.com/in/#{user.linkedin}"
-    end
+    social_media_link.new(user).linkedin_url
   end
+
   def lesson_time(user, lesson)
-    time = user.lesson_completion_time(lesson)
-    time.to_formatted_s(:long)
+    user.lesson_completion_time(lesson).to_formatted_s(:long)
+  end
+
+  private
+
+  def social_media_link
+    SocialMediaLink
   end
 end
---
+++ spec/services/social_media_link_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper'
+
+RSpec.describe SocialMediaLink do
+  subject(:social_media_link) { SocialMediaLink.new(user) }
+
+  let(:user) {
+    double(
+      'User',
+      twitter: user_twitter,
+      facebook: user_facebook,
+      linkedin: user_linkedin,
+      github: user_github,
+    )
+  }
+  let(:user_twitter) { 'odinstudent' }
+  let(:user_facebook) { 'odinstudent' }
+  let(:user_linkedin) { 'odinstudent' }
+  let(:user_github) { 'odinstudent' }
+
+  describe '#twitter_url' do
+
+    it 'returns the users twitter url' do
+      expect(social_media_link.twitter_url).
+        to eql('http://www.twitter.com/odinstudent')
+    end
+
+    context 'when the twitter handle starts with @' do
+      let(:user_twitter) { '@odinstudent' }
+
+      it 'returns the twitter url correctly formatted' do
+        expect(social_media_link.twitter_url).
+          to eql('http://www.twitter.com/odinstudent')
+      end
+    end
+
+    context 'when the user does not have a twitter username' do
+      let(:user_twitter) { nil }
+
+      it 'returns nil' do
+        expect(social_media_link.twitter_url).to eql(nil)
+      end
+    end
+  end
+
+  describe '#facebook_url' do
+
+    it 'returns the users formatted facebook url' do
+      expect(social_media_link.facebook_url).
+        to eql('https://www.facebook.com/odinstudent')
+    end
+
+    context 'when user does not have a facebook username' do
+      let(:user_facebook) { nil }
+
+      it 'returns nil' do
+        expect(social_media_link.facebook_url).to eql(nil)
+      end
+    end
+  end
+
+  describe '#linkedin_url' do
+
+    it 'returns the users formatted linkedin url' do
+      expect(social_media_link.linkedin_url).
+        to eql('https://www.linkedin.com/in/odinstudent')
+    end
+
+    context 'when user does not have a linkedin username' do
+      let(:user_linkedin) { nil }
+
+      it 'returns nil' do
+        expect(social_media_link.linkedin).to eql(nil)
+      end
+    end
+  end
+
+  describe '#github_url' do
+    it 'returns the users github url' do
+      expect(social_media_link.github_url).
+        to eql('http://www.github.com/odinstudent')
+    end
+
+    context 'when the github username starts with http' do
+      let(:user_github) { 'http://www.github.com/odinstudent' }
+
+      it 'returns the twitter url correctly formatted' do
+        expect(social_media_link.github_url).
+          to eql('http://www.github.com/odinstudent')
+      end
+    end
+
+    context 'when the user does not have a github username' do
+      let(:user_github) { nil }
+
+      it 'returns nil' do
+        expect(social_media_link.github_url).to eql(nil)
+      end
+    end
+  end
+end
