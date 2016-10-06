a4b40665e81b5863821fa92101112f36f2c3594a
Kevin <kevinmulhern23@gmail.com>
2016-10-06 22:02:56 +0100

    Initial refactoring of bootstrap flash helper

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,16 +1,19 @@
 module BootstrapFlashHelper
 
-  ALERT_TYPES = [:error, :info, :success, :warning]
-
   def bootstrap_flash
-    flash_messages = []
     flash.each do |type, message|
+      puts "*" * 100
+      puts type
+      puts message
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
-      next if message.blank?
+      # convert type to symbol (it might be a string)
       type = type.to_sym
+      # convert to bootstrap flash messages
       type = :success if type == :notice
       type = :error   if type == :alert
-      next unless ALERT_TYPES.include?(type)
+      # skip if alert types does not include type
+
+
       Array(message).each do |msg|
         text = content_tag(:div,
                            content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
@@ -19,5 +22,15 @@ module BootstrapFlashHelper
       end
     end
     flash_messages.join("\n").html_safe
+  end
+
+  private
+
+  def alert_types
+    [:error, :info, :success, :warning]
+  end
+
+  def flash_messages
+    @flash_messages ||= []
   end
 end
---
+++ spec/helpers/bootstrap_flash_helper_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe BootstrapFlashHelper do
+
+  describe '#bootstrap_flash' do
+
+  end
+end
