a7fd0ea52992bfe5b4f1bd32b08ee12aa0b82f35
Kevin <kevinmulhern23@gmail.com>
2016-10-07 12:51:30 +0100

    added unit test for boot_strap_class_for

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+
+  describe '#bootstrap_class_for' do
+    let(:flash_type) { 'alert' }
+
+    it 'returns the bootstrap alert class' do
+      expect(helper.bootstrap_class_for(flash_type)).to eql('alert-error')
+    end
+
+    context 'when flash type is notice' do
+      let(:flash_type) { 'notice' }
+
+      it 'returns the bootstrap success class' do
+        expect(helper.bootstrap_class_for(flash_type)).to eql('alert-success')
+      end
+    end
+
+    context 'when flash type is error' do
+      let(:flash_type) { 'error' }
+
+      it 'returns the bootstrap success class' do
+        expect(helper.bootstrap_class_for(flash_type)).to eql('alert-error')
+      end
+    end
+  end
+end
