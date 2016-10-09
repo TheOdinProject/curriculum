732fe7af1896faae89c279aefd74d5b4657d23ef
Kevin <kevinmulhern23@gmail.com>
2016-10-09 10:09:45 +0100

    refactored admin flash helper

--- spec/helpers/admin_flash_helper_spec.rb
+++ spec/helpers/admin_flash_helper_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 describe AdminFlashHelper do
 
-  describe "#disabled_flash_ids_from_cookies" do
+  describe '#disabled_flash_ids_from_cookies' do
+    let(:cookies) {
+      {
+        'admin_flash_10' => 'disabled',
+        'admin_flash_20' => 'disabled',
+        'not_a_flash_15' => 'disabled',
+      }
+    }
 
-    let(:cookies){ {"admin_flash_10" => "disabled","admin_flash_20"=>"disabled","not_a_flash_15"=>"disabled"} }
-
-    it "should grab the flash IDs" do
-      expect(disabled_flash_ids_from_cookies).to include(10)
+    it 'returns the disabled flash ids' do
+      expect(disabled_flash_ids_from_cookies).to eql([10, 20])
     end
-
-    it "should NOT grab an improperly formatted flash ID" do
-      expect(disabled_flash_ids_from_cookies).not_to include(15)
-    end
-
   end
-
 end
