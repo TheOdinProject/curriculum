3bd7d35249ae180713c4244c3d6eb5559a09a07e
Johnny Hu <jgeehen.hu@gmail.com>
2016-10-19 15:58:02 -0700

    Refactor sections model spec file
    Addresses Issue #263
    
    Added tests for model validations and associations. Removed test for duplicate positions and FactoryGirl variables. I extracted the methods in the before action and moved them to their respectively model's initialization, thus removing the need for the before action altogether.

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe Section do
-  let(:attrs) { FactoryGirl.attributes_for(:section) }
-  let(:course) { double("course") }
-  subject(:section) { Section.new(attrs) }
-  before do
-    allow(course).to receive("id"){ 1 }
-    subject.course_id = course.id
-  end
+  let(:course) { double("Course", :id => 1) }
+
+  subject(:section) { Section.new(
+    :title => "test section",
+    :title_url => "testsection.url.com",
+    :description => "some test description",
+    :position => 2,
+    :course_id => course.id 
+  )}
 
   it { is_expected.to respond_to(:title) }
   it { is_expected.to respond_to(:title_url) }
@@ -16,16 +18,11 @@ describe Section do
   it { is_expected.to respond_to(:course_id) }
 
   # Associations
-  it { is_expected.to respond_to(:course) }
-  it { is_expected.to respond_to(:lessons) }
-
+  it { is_expected.to belong_to(:course) }
+  it { is_expected.to have_many(:lessons) }
+  
+  # Validations
+  it { is_expected.to validate_uniqueness_of(:position).with_message('Section position has already been taken') }
   it { is_expected.to be_valid }
-
-  it "shouldn't allow duplicate positions" do
-    s2 = Section.new(attrs)
-    s2.course_id = course.id
-    s2.save
-    expect(subject).not_to be_valid
-  end
 
 end
