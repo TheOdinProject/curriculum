6cafaa0eb2bf437e0b28146e662a39ddc3d1c5b4
Kevin <kevinmulhern23@gmail.com>
2016-10-19 21:35:02 +0100

    Dropped unused database tables
    * content_activation table
    * content_bucket table
    * user_prefs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017232509) do
+ActiveRecord::Schema.define(version: 20161019203200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,23 +33,6 @@ ActiveRecord::Schema.define(version: 20161017232509) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "content_activations", force: :cascade do |t|
-    t.integer  "user_id",           null: false
-    t.integer  "content_bucket_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "content_activations", ["user_id", "content_bucket_id"], name: "index_content_activations_on_user_id_and_content_bucket_id", unique: true, using: :btree
-
-  create_table "content_buckets", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "content_buckets", ["name"], name: "index_content_buckets_on_name", unique: true, using: :btree
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -120,14 +103,6 @@ ActiveRecord::Schema.define(version: 20161017232509) do
   add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
   add_index "sections", ["position"], name: "index_sections_on_position", using: :btree
   add_index "sections", ["title_url"], name: "index_sections_on_title_url", using: :btree
-
-  create_table "user_prefs", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_prefs", ["user_id"], name: "index_user_prefs_on_user_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
