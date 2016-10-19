982143299b2c2143f8578616f855318a9329ad60
Johnny Hu <jgeehen.hu@gmail.com>
2016-10-19 09:37:23 -0700

    Hardcode IDs into subject; remove let() methods and comment

--- spec/models/lesson_completion_spec.rb
+++ spec/models/lesson_completion_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 describe LessonCompletion do
 
-  let!(:user)  { double("User",    :id => 1) }
-  let!(:lesson){ double("Lessons", :id => 1) }
-
   subject(:lesson_completion) { LessonCompletion.new(
-    :student_id => user.id,
-    :lesson_id => lesson.id
+    :student_id => 1,
+    :lesson_id => 1
   )}
 
   it { is_expected.to be_valid }
@@ -18,7 +15,5 @@ describe LessonCompletion do
   it { is_expected.to validate_uniqueness_of(:student_id).scoped_to(:lesson_id) }
   it { is_expected.to belong_to(:student) }
   it { is_expected.to belong_to(:lesson) }
-
-  # Also test that they delete when the lesson or user are deleted?
 
 end
