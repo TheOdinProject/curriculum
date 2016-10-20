e9365bb5545d38c323d445a9f7dc8116f8e805e1
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-21 04:14:09 +0530

    move mocks from before to let

--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -9,10 +9,10 @@ describe Ad do
 
   describe '.ve_banner' do
     let(:banner_ad_attrs) { { style: 'banner', client: 've', category: 'b2c_ab' } }
+    let(:banner_ads) { double('BannerAds') }
     let(:banner_ad) { double('BannerAd', banner_ad_attrs) }
 
     before do
-      banner_ads = double
       allow(Ad).to receive(:where).with(banner_ad_attrs).and_return(banner_ads)
       allow(banner_ads).to receive(:sample).and_return(banner_ad)
     end
@@ -24,10 +24,10 @@ describe Ad do
 
   describe '.ve_box' do
     let(:box_ad_attrs) { { style: 'box', client: 've', category: 'b2c_ab' } }
+    let(:box_ads) { double('BoxAds') }
     let(:box_ad) { double('BoxAd', box_ad_attrs) }
 
     before do
-      box_ads = double
       allow(Ad).to receive(:where).with(box_ad_attrs).and_return(box_ads)
       allow(box_ads).to receive(:sample).and_return(box_ad)
     end
