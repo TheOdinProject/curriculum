166371e52547a9ffc322bd727973feca61d5ebac
Kevin <kevinmulhern23@gmail.com>
2016-11-06 03:21:37 +0000

    fixed identenation in lesson completions controller

--- app/controllers/lesson_completions_controller.rb
+++ app/controllers/lesson_completions_controller.rb
@@ -1,13 +1,12 @@
 class LessonCompletionsController < ApplicationController
-  
   before_action :authenticate_request
   before_action :lookup_lesson
-  
+
   def create
     new_lesson_completion.save
     render :create, formats: [:js]
   end
-  
+
   def destroy
     lesson_completion.destroy
     render :create, formats: [:js]
@@ -15,19 +14,19 @@ class LessonCompletionsController < ApplicationController
 
   private
 
-    def lesson_completion
-      LessonCompletion.where(student_id: current_user.id, lesson_id: @lesson.id).first
-    end
+  def lesson_completion
+    LessonCompletion.where(student_id: current_user.id, lesson_id: @lesson.id).first
+  end
 
-    def new_lesson_completion
-      LessonCompletion.new(student_id: current_user.id, lesson_id: @lesson.id)
-    end
+  def new_lesson_completion
+    LessonCompletion.new(student_id: current_user.id, lesson_id: @lesson.id)
+  end
 
-    def lookup_lesson
-      @lesson = Lesson.find(params[:lesson_id])
-    end
+  def lookup_lesson
+    @lesson = Lesson.find(params[:lesson_id])
+  end
 
-    def authenticate_request
-      head :unauthorized unless user_signed_in?
-    end
+  def authenticate_request
+    head :unauthorized unless user_signed_in?
+  end
 end
