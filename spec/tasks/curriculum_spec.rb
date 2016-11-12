2b4557cfebe217091d074c3dc26cf7b715d481e6
Arun Kumar <arunkumarm1995@gmail.com>
2016-11-12 05:48:33 +0530

    Refactor curriculum update rake task and spec

--- spec/tasks/curriculum_spec.rb
+++ spec/tasks/curriculum_spec.rb
@@ -1,24 +1,23 @@
-require "rails_helper"
-require "rake"
+require 'rails_helper'
+require 'rake'
 
-describe ":curriculum" do
+describe ':curriculum' do
   before :all do
-    Rake.application.rake_require "tasks/curriculum"
+    Rake.application.rake_require 'tasks/curriculum'
     Rake::Task.define_task(:environment)
   end
 
-  describe "curriculum:update_content" do
-    let(:lesson) { FactoryGirl.create(:lesson, url: "/README.md", content: nil) }
+  describe 'curriculum:update_content' do
+    let!(:lesson) { FactoryGirl.create(:lesson, url: '/README.md', content: nil) }
     let(:curriculum_update_content) do
-      lesson
-      Rake::Task["curriculum:update_content"].reenable
+      Rake::Task['curriculum:update_content'].reenable
 
-      VCR.use_cassette("lesson_content") do
-        Rake.application.invoke_task "curriculum:update_content"
+      VCR.use_cassette('lesson_content') do
+        Rake.application.invoke_task 'curriculum:update_content'
       end
     end
 
-    it "updates the lesson with content" do
+    it 'updates the lesson with content' do
       curriculum_update_content
       expect(lesson.reload.content).not_to be_nil
     end
