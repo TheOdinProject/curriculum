41f84be20dc2cae0344cca76f4e90581c18bc56f
Kevin <kevinmulhern23@gmail.com>
2016-11-14 19:59:22 +0000

    added host to prod initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  Rails.application.routes.default_url_options[:host] = 'theodinproject.com'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
