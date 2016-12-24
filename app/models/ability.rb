739d213c238eda026e1485a62ec7cf15a86c619f
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-24 12:15:13 +0530

    Allow admin to update and destroy project submissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,31 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    @user = user || User.new
+    abilities
+  end
 
-    can :update, :all, id: user.id
+  def abilities
+    common_abilities
+
+    if @user.admin?
+      admin_abilities
+    else
+      standard_user_abilities
+    end
+  end
+
+  def common_abilities
+    can :update, UserDecorator, id: @user.id
+  end
+
+  def admin_abilities
+    can [:update, :destroy], Project
+  end
+
+  def standard_user_abilities
+    can [:update, :destroy], Project do |project|
+      project.user == @user
+    end
   end
 end
