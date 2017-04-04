fca93bd20d3df24be50f4e8c9eb353143226696e
Arun Kumar <arunkumarm1995@gmail.com>
2017-04-04 21:56:25 +0530

    Redesign FAQ page

---
+++ app/assets/javascripts/faq.js
@@ -0,0 +1,21 @@
+function setup() {
+  $('#faq-accordion').on('show.bs.collapse', function (event) {
+    console.log(event.target);
+    var openedCardSelector = getOpenedCardSelector(event.target);
+    $(openedCardSelector + ' .expander').hide();
+    $(openedCardSelector + ' .minimizer').show();
+  });
+
+  $('#faq-accordion').on('hide.bs.collapse', function (event) {
+    var openedCardSelector = getOpenedCardSelector(event.target);
+    $(openedCardSelector + ' .minimizer').hide();
+    $(openedCardSelector + ' .expander').show();
+  });
+}
+
+function getOpenedCardSelector(element) {
+  var openedCardId = $(element).attr('id');
+  return '.faq-card a[href="#' + openedCardId + '"]';
+}
+
+$(document).on('turbolinks:load', setup);
