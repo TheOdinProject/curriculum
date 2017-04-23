35a4ba4fa958de4d7cd051ea761e25b30ae48778
wanyamaman <will.i.yeezy@gmail.com>
2017-04-23 20:27:09 +0300

    Redesign contributing page

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170318161553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "admin_flashes", force: :cascade do |t|
     t.datetime "created_at"
