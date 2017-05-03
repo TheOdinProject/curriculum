a90bdfd3eeb580e632208d4692656b2b938b92dc
Arun Kumar <arunkumarm1995@gmail.com>
2017-05-04 05:28:21 +0530

    Remove click event listener

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -10,15 +10,4 @@ document.addEventListener('DOMContentLoaded', function() {
     logoImg.classList.toggle('expanded')
     logo.classList.toggle('expanded')
   })
-
-  // Collapses the navbar if a click is made outside it
-  document.addEventListener('click', function(event) {
-    var navbar = document.querySelector('.navbar-collapse')
-    var collapseButton = document.querySelector('.navbar-toggler-icon + .close')
-
-    var navbarOpened = navbar.classList.contains('show')
-    var linkClicked = event.target.tagName.toLowerCase() === 'a'
-
-    if (navbarOpened && !linkClicked) collapseButton.click()
-  })
 })
