a22de987ccb6ad65bef273c03815ba6368ce12fa
Kevin Mulhern <kevin.mulhern@onthebeach.co.uk>
2017-05-13 10:02:31 +0100

    Created a bottom call to action partial

---
+++ spec/helpers/button_helper_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe ButtonHelper do
+  describe '#sign_up_button' do
+    let(:sign_up_button) {
+      '<a class="button button--primary" href="/sign_up?ref=success_stories">Sign Up</a>'
+    }
+
+    it 'returns a sign up button' do
+      expect(helper.sign_up_button).to eq(sign_up_button)
+    end
+  end
+
+  describe '#gitter_button' do
+    let(:gitter_button) {
+      '<a class="button button--secondary" target="__blank" href="https://gitter.im/TheOdinProject/theodinproject">Open Gitter</a>'
+    }
+
+    before do
+      allow(helper).to receive(:chat_link).and_return('https://gitter.im/TheOdinProject/theodinproject')
+    end
+
+    it 'returns a gitter button' do
+      expect(helper.gitter_button).to eql(gitter_button)
+    end
+  end
+
+  describe '#contribute_button' do
+    let(:contribute_button) {
+      '<a class="button button--primary" href="/contributing">Learn how to contribute</a>'
+    }
+
+    it 'returns a contribute button' do
+      expect(helper.contribute_button).to eql(contribute_button)
+    end
+  end
+end
