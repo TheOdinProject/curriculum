b57c10e5cc5622f3f1f2827e8d0a93b4a5e121d8
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-05-26 16:29:56 +0700

    Refactored checkbox form input using a custom boolean input class

---
+++ app/inputs/custom_boolean_input.rb
@@ -0,0 +1,15 @@
+class CustomBooleanInput < SimpleForm::Inputs::BooleanInput
+  def input_html_classes
+    super.push("normal-checkbox")
+  end
+
+  private
+
+
+    def build_check_box(unchecked_value, options)
+      @builder.check_box(attribute_name, options, checked_value, unchecked_value) +
+      @builder.label(attribute_name, class: "custom-checkbox") do
+        content_tag(:div)
+      end
+    end
+end
