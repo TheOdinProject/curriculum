e6cda981b1dad3e49492646143a392719de6bda3
Arun Kumar <arunkumarm1995@gmail.com>
2017-05-31 02:21:53 +0530

    Move lesson actions into a partial

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,15 +1,2 @@
 module LessonsHelper
-  def course_url(lesson)
-    link_to 'View Course',
-      course_path(lesson.course.title_url, ref: 'lnav'),
-      title: "Go back to '#{lesson.course.title}'",
-      class: 'button button--secondary'
-  end
-
-  def next_lesson_url(lesson)
-    link_to 'Next Lesson',
-      course_lesson_path(lesson.course, lesson.next_lesson, ref: 'lnav'),
-      title: "Move on to '#{lesson.next_lesson.title}'",
-      class: 'button button--secondary'
-  end
 end
