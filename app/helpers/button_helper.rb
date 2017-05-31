accce1f75063933f9c8df7ffd6e08bd912d3c560
Oscar Yih <oscaryih@hotmail.com>
2017-05-31 16:22:42 -0700

    Fix CTA button for current_user

--- app/helpers/button_helper.rb
+++ app/helpers/button_helper.rb
@@ -3,6 +3,10 @@ module ButtonHelper
     link_to 'Sign Up', sign_up_path(ref: 'success_stories'), class: 'button button--primary'
   end
 
+  def curriculum_button
+  	link_to 'Curriculum', curriculum_path(ref: 'success_stories'), class: 'button button--primary'
+  end
+
   def gitter_button
     link_to 'Open Gitter', chat_link, class: 'button button--secondary', target: '_blank'
   end
