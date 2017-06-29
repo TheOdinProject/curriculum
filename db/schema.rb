18cff05bf5eca8f66061229a33b82d590ca1f512
Kevin <kevinmulhern23@gmail.com>
2017-06-29 19:15:38 +0100

    Removed estimated completion date from the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615185649) do
+ActiveRecord::Schema.define(version: 20170629181228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,12 +121,12 @@ ActiveRecord::Schema.define(version: 20170615185649) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                         default: "",    null: false
-    t.string   "encrypted_password",            default: "",    null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                 default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -142,7 +142,7 @@ ActiveRecord::Schema.define(version: 20170615185649) do
     t.string   "google_plus"
     t.string   "skype"
     t.string   "screenhero"
-    t.boolean  "legal_agreement",               default: false, null: false
+    t.boolean  "legal_agreement",        default: false, null: false
     t.datetime "legal_agree_date"
     t.string   "provider"
     t.string   "uid"
@@ -150,8 +150,7 @@ ActiveRecord::Schema.define(version: 20170615185649) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.boolean  "admin",                         default: false, null: false
-    t.date     "learning_goal_completion_date"
+    t.boolean  "admin",                  default: false, null: false
     t.string   "avatar"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
