31a651c322922d1f55969e5a221dfecbeaa02090
Renz Iver Baliber <renziverdb@gmail.com>
2017-07-05 11:06:14 +0800

    Redesign dashboard

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,5 +7,8 @@ module UsersHelper
   def lesson_time(user, lesson)
     user.lesson_completion_time(lesson).to_formatted_s(:long)
   end
-end
 
+  def set_learning_goal
+    link_to "set a learning goal in your settings", edit_user_registration_path, class: 'accent'
+  end
+end
