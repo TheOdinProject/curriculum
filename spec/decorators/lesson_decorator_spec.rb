7da36f3e3233477af9e999a1087d6c799f136200
KevinMulhern <kevinmulhern23@gmail.com>
2017-07-28 12:17:35 +0100

    Clean up Decorators
    This cleans up all of the decorators by removing methods that are no loner used.

--- spec/decorators/lesson_decorator_spec.rb
+++ spec/decorators/lesson_decorator_spec.rb
@@ -3,22 +3,14 @@ require 'rails_helper'
 RSpec.describe LessonDecorator do
   subject(:lesson_decorator) { LessonDecorator.new(lesson) }
 
-  let(:lesson) { double('Lesson', url: url, title: title ) }
-  let(:url) { '/web_development_101/jquery_basics.md' }
-  let(:title) { 'What a Web Developer Does' }
+  let(:lesson) { double('Lesson', url: lesson_url) }
+  let(:lesson_url) { '/web_development_101/jquery_basics.md' }
 
   describe '#github_url' do
     let(:root) { 'https://github.com/TheOdinProject/curriculum/tree/master' }
 
-
     it 'returns a url to the lesson on github' do
-      expect(lesson_decorator.github_url).to eql(root + url)
-    end
-  end
-
-  describe '#title_url_format' do
-    it 'returns url with multiple words' do
-      expect(lesson_decorator.title_url_format).to eq 'What+a+Web+Developer+Does'
+      expect(lesson_decorator.github_url).to eql(root + lesson_url)
     end
   end
 end
