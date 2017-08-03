459f95b93f33a01a2e0f2ab33191f77050f923e2
Andy Duss <mindovermiles262@gmail.com>
2017-08-03 08:19:33 -0700

    User Dropdown open on hover

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,8 +1,9 @@
 document.addEventListener('turbolinks:load', function() {
   var logoText = document.querySelector('.logo__text')
-
   var navToggle = document.querySelector('.navbar-toggler')
   var modalClose = document.querySelector('.close')
+
+  var userDropdown = document.querySelector('.dropdown')
 
   // Clears Logo text in modal
   navToggle.addEventListener('click', function() {
@@ -13,4 +14,13 @@ document.addEventListener('turbolinks:load', function() {
   modalClose.addEventListener('click', function() {
     logoText.classList.toggle('expanded');
   })
+
+  // Open User Dropdown on Hover, hide on mouseout
+  userDropdown.addEventListener('mouseover', function() {
+    userDropdown.classList.add('show')
+  })
+  userDropdown.addEventListener('mouseout', function() {
+    userDropdown.classList.remove('show')
+  })
+
 })
