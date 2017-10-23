42e6c332bb8b2954caac2541e329a8476bee9a6f
Kevin Mulhern <kevinmulhern23@gmail.com>
2017-10-23 13:19:39 +0100

    Feature/fixes galore (#783)
    * debugging
    
    * Fix edge cases with resume button going for the last lesson in the course if its already been completed
    
    * fix the toc

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,4 +2,14 @@ class UserDecorator < ApplicationDecorator
   def has_projects?
     projects.exists?
   end
+
+  def lesson_completions_for(course)
+    CompletedLessons.new(course, user).for_course
+  end
+
+  private
+
+  def user
+    __getobj__
+  end
 end
