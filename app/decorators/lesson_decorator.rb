106bccecf63ccc6acbfbb0c91a4fe8006579ecac
Andy Duss <mindovermiles262@gmail.com>
2017-11-29 08:19:57 -0800

    remove unused decorator

--- app/decorators/lesson_decorator.rb
+++ app/decorators/lesson_decorator.rb
@@ -8,11 +8,6 @@ class LessonDecorator < ApplicationDecorator
     end
   end
 
-  def github_url
-    # Change lesson pull location in services/lesson_content_importer
-    'https://github.com/TheOdinProject/curriculum/tree/master' + url
-  end
-
   def next_lesson
     FindLesson.new(lesson, course).next_lesson
   end
