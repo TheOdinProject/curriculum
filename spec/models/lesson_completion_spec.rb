a2f11db6e463a3276a645042db664baf54f46162
Kevin <kevinmulhern23@gmail.com>
2018-03-11 17:53:53 +0000

    Hot fix: Remove unique validation for student id and lesson id combo on lesson comletions model, there seems to be a race condition happening between this validation and the unique constraint in the database

--- spec/models/lesson_completion_spec.rb
+++ spec/models/lesson_completion_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe LessonCompletion do
 
   it { is_expected.to validate_presence_of(:student_id) }
   it { is_expected.to validate_presence_of(:lesson_id) }
-  it do
-    is_expected.to validate_uniqueness_of(:student_id).scoped_to(:lesson_id)
-  end
+
   it { is_expected.to belong_to(:student) }
   it { is_expected.to belong_to(:lesson) }
 end
