86c7205aca1de42513f53bae264109be50bc1630
Kevin <kevinmulhern23@gmail.com>
2018-04-14 23:28:01 +0100

    Moved the private methods in the order they are called from public methods to make the course controller slightly easier to read

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,15 +10,15 @@ class CoursesController < ApplicationController
 
   private
 
-  def course
-    Course.includes(:lessons, sections: [:lessons]).friendly.find(params[:id])
-  end
-
   def decorated_courses
     ordered_courses.map { |course| CourseDecorator.new(course) }
   end
 
   def ordered_courses
     Course.order(:position)
+  end
+
+  def course
+    Course.includes(:lessons, sections: [:lessons]).friendly.find(params[:id])
   end
 end
