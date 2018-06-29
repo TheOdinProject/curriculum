bc5393bb61328a116aa993c6e6bdc5f259df8ae5
Rhys <105ron@users.noreply.github.com>
2018-06-29 22:23:02 +0800

    Update application.js (#915)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery3
 //= require toastr
 //= require validate
 //= require tether
