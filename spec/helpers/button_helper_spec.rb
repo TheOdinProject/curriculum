b85bf14d1e468e958fbfd0c5d0d76a1d651fce48
Briggs Elsperger <briggse@protonmail.com>
2020-03-27 17:45:59 -0700

    Updates discord link

--- spec/helpers/button_helper_spec.rb
+++ spec/helpers/button_helper_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ButtonHelper do
 
   describe '#chat_button' do
     let(:chat_button) {
-      '<a class="button button--secondary" target="_blank" rel="noreferrer" href="https://discord.gg/hvqVr6d">Open Discord</a>'
+      '<a class="button button--secondary" target="_blank" rel="noreferrer" href="https://discord.gg/V75WSQG">Open Discord</a>'
     }
 
     before do
