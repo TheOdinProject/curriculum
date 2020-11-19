f03dd9570826c01a5979198e5874d95d61a4baa6
Leonard Soai-Van <leo.soaivan@gmail.com>
2020-11-19 17:06:19 -0500

    Reduce initial tests to focus on display

--- app/javascript/components/project-submissions/components/__tests__/visible-toggle.test.js
+++ app/javascript/components/project-submissions/components/__tests__/visible-toggle.test.js
@@ -3,42 +3,24 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import VisibleToggle from '../visible-toggle';
 
 describe('VisibleToggle', () => {
-  test('Toggles from public to private on click', () => {
-    const publicSubmission = {
-      id: '1',
-      is_public: true,
-      live_preview_url: 'https://www.google.com',
-      repo_url: 'https://www.google.com',
-    }
-
+  test('Indicates that a submission is public', () => {
     render(
       <VisibleToggle
-        submission={publicSubmission}
+        submission={{ is_public: true }}
         handleVisibleToggle={() => {}}
       />
-    )
-
-    fireEvent.click(screen.getByLabelText('Toggle visibility'));
+    );
 
     expect(screen.getByLabelText('Toggle visibility')).toHaveClass('submissions__public-icon--visible');
   });
 
-  test('Toggles from public to private on click', () => {
-    const privateSubmission = {
-      id: '1',
-      is_public: false,
-      live_preview_url: 'https://www.google.com',
-      repo_url: 'https://www.google.com',
-    }
-
+  test('Indicates that a submission is private', () => {
     render(
       <VisibleToggle
-        submission={privateSubmission}
+        submission={{ is_public: false }}
         handleVisibleToggle={() => {}}
       />
     )
-
-    fireEvent.click(screen.getByLabelText('Toggle visibility'));
 
     expect(screen.getByLabelText('Toggle visibility')).not.toHaveClass('submissions__public-icon--visible');
   });
