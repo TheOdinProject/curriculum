38697352020e2781e218f15281dfd1978079be30
Alex Younger <alexanderyounger@gmail.com>
2020-11-30 17:14:53 -0500

    convert tabs to spaces

--- app/javascript/components/project-submissions/components/__tests__/submission-title.test.js
+++ app/javascript/components/project-submissions/components/__tests__/submission-title.test.js
@@ -4,25 +4,25 @@ import '@testing-library/jest-dom';
 import SubmissionTitle from '../submission-title';
 
 const submission = {
-	lesson_path: 'test_path',
-	lesson_title: 'Test Title',
-	user_name: 'TestUser',
+  lesson_path: 'test_path',
+  lesson_title: 'Test Title',
+  user_name: 'TestUser',
 };
 
 describe('Dashboard view true', () => {
-	it('shows the lesson title in dashboard view', () => {
-		render(<SubmissionTitle submission={submission} isDashboardView={true} />);
-		expect(screen.getByText('Test Title')).toBeInTheDocument();
-	});
-	it('has a link to the project in dashboard view', () => {
-		render(<SubmissionTitle submission={submission} isDashboardView={true} />);
-		expect(screen.getByText('Test Title')).toHaveAttribute('href', 'test_path');
-	});
+  it('shows the lesson title in dashboard view', () => {
+    render(<SubmissionTitle submission={submission} isDashboardView={true} />);
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+  });
+  it('has a link to the project in dashboard view', () => {
+    render(<SubmissionTitle submission={submission} isDashboardView={true} />);
+    expect(screen.getByText('Test Title')).toHaveAttribute('href', 'test_path');
+  });
 });
 
 describe('Dashboard view false', () => {
-	it('returns username if not in dashboard mode', () => {
-		render(<SubmissionTitle submission={submission} />);
-		expect(screen.getByText('TestUser')).toBeInTheDocument();
-	});
+  it('returns username if not in dashboard mode', () => {
+    render(<SubmissionTitle submission={submission} />);
+    expect(screen.getByText('TestUser')).toBeInTheDocument();
+  });
 });
