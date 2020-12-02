dac7e9850a78604f6f75ed71d1188432196f0630
Aayush Kumar Sahu <aayushmau5@gmail.com>
2020-12-02 13:04:07 +0530

    Change tests to check for children props

--- app/javascript/components/project-submissions/components/__tests__/modal.test.js
+++ app/javascript/components/project-submissions/components/__tests__/modal.test.js
@@ -4,29 +4,31 @@ import Modal from "../modal";
 
 describe("Modal", () => {
   test("Checks for the specified classname on modal when the modal is not shown", () => {
-    const { getByLabelText } = render(
+    const { queryByLabelText, getByLabelText } = render(
       <Modal 
         handleClose={() => {}} 
         show={false}
       >
-        {" "}
+        <div aria-label="modal-data" ></div>
       </Modal>
     );
 
     expect(getByLabelText("modal--hidden")).toHaveClass("react-modal--hidden");
+    expect(queryByLabelText("modal-data")).toBeFalsy();
   });
 
   test("Checks for the specified classname on modal when the modal is shown", () => {
-    const { getByLabelText } = render(
+    const { queryByLabelText, getByLabelText } = render(
       <Modal 
         handleClose={() => {}} 
         show={true}
       >
-        {" "}
+        <div aria-label="modal-data" ></div>
       </Modal>
     );
 
     expect(getByLabelText("modal--shown")).toHaveClass("react-modal");
+    expect(queryByLabelText("modal-data")).toBeTruthy();
   });
 
   test("Calls close handler when clicked", () => {
