16f96d2744affd28047d6d411bee7c6929680269
David Majowka <36450233+DoviMaj@users.noreply.github.com>
2020-12-11 15:11:34 +0200

    Update 05_html_css_course_seeds.rb

--- db/seeds/05_html_css_course_seeds.rb
+++ db/seeds/05_html_css_course_seeds.rb
@@ -12,7 +12,7 @@ lesson_position = 0
 course = create_or_update_course(
   title: 'HTML and CSS',
   title_url: 'HTML and CSS'.parameterize,
-  description: "Good web design doesn't happen by accident. Learn how to make all that work you've done on the backend look great in a web browser! You'll be equipped to deeply understand and create your own design frameworks.",
+  description: "Good web design doesn't happen by accident. This course takes a deeper look at front-end design and development, expanding on what is covered in Foundations. You'll learn how to design and develop websites that look great in any device and you'll be equipped to deeply understand and create your own responsive design framework!",
   position: course_position
 )
 
