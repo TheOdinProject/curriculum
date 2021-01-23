dc3ab60e133abfeacf6af8183f611ca0bc9037d9
Ranko Liang <rankoliang@gmail.com>
2021-01-23 11:08:03 -0500

    Add flag form tests when submission is successful

--- app/javascript/components/project-submissions/components/__tests__/flag-form.test.jsx
+++ app/javascript/components/project-submissions/components/__tests__/flag-form.test.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  fireEvent, render, screen, act,
+} from '@testing-library/react';
 import FlagForm from '../flag-form';
 
 describe('FlagForm', () => {
   describe('when the user is not signed in', () => {
     test('prompts the user to sign in', () => {
-      const { getByRole } = render((
+      render((
         <FlagForm
           userId={null}
           onSubmit={() => {}}
@@ -13,7 +15,35 @@ describe('FlagForm', () => {
         />
       ));
 
-      expect(getByRole('link', { name: /sign in/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument();
+    });
+  });
+
+  describe('when submission is successful', () => {
+    beforeEach(async () => {
+      render(
+        <FlagForm
+          userId={43}
+          onSubmit={() => {}}
+          submission={{}}
+        />,
+      );
+
+      await act(async () => {
+        fireEvent.change(screen.getByRole('textbox'), {
+          target: { value: 'Submission has malicious content' },
+        });
+
+        fireEvent.click(screen.getByRole('button', { name: /flag/i }));
+      });
+    });
+
+    test('shows a thank you message', () => {
+      expect(screen.getByRole('heading', { name: /thank/i })).toBeInTheDocument();
+    });
+
+    test('shows intent to review', () => {
+      expect(screen.getByText(/review/i)).toBeInTheDocument();
     });
   });
 });
