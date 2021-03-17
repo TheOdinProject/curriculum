d515821a95113b3c5334964cd022f3d7ef54ece1
Omar Moataz <omarmoataz@outlook.com>
2021-03-17 22:44:29 +0200

    Added tests for submissions that cover user interactions like report, edit, live preview, and view code actions on depending on whether the user owns the submission or not.

---
+++ app/javascript/components/project-submissions/components/__tests__/submission.test.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import ProjectSubmissionContext from '../../ProjectSubmissionContext';
+import Submission from '../submission';
+
+const defaultSubmission = {
+  user_name: 'Example User',
+  repo_url: 'http://example.com/repo',
+  live_preview_url: 'https://google.com',
+  is_public: true,
+  lesson_has_live_preview: true,
+  user_id: 1,
+};
+
+const renderSubmissionComponent = (submission, userId = 1, onFlag) => render(
+  <ProjectSubmissionContext.Provider value={{
+    userId,
+  }}
+  >
+    <Submission
+      submission={submission}
+      handleUpdate={jest.fn()}
+      onFlag={onFlag || jest.fn()}
+      handleDelete={jest.fn()}
+      handleLikeToggle={jest.fn()}
+    />
+    ,
+  </ProjectSubmissionContext.Provider>,
+);
+
+describe('Submission', () => {
+  let queryByTestId;
+
+  afterAll(() => {
+    jest.resetModules();
+  });
+
+  describe('Tests for all submissions', () => {
+    test('Renders live preivew with valid live_preview_url', () => {
+      ({ queryByTestId } = renderSubmissionComponent({
+        ...defaultSubmission,
+        live_preview_url: 'https://google.com',
+      }));
+
+      const livePreviewLink = queryByTestId('live-preview-btn');
+      expect(livePreviewLink).toBeInTheDocument();
+      expect(livePreviewLink).toHaveAttribute('target', '_blank');
+    });
+
+    test('Does not render live preivew without live_preview_url', () => {
+      ({ queryByTestId } = renderSubmissionComponent({
+        ...defaultSubmission,
+        live_preview_url: '',
+      }));
+
+      const livePreviewLink = queryByTestId('live-preview-btn');
+      expect(livePreviewLink).not.toBeInTheDocument();
+    });
+
+    test('Renders view code link', () => {
+      ({ queryByTestId } = renderSubmissionComponent(defaultSubmission));
+
+      const viewCodeLink = queryByTestId('view-code-btn');
+      expect(viewCodeLink).toBeInTheDocument();
+      expect(viewCodeLink).toHaveAttribute('target', '_blank');
+    });
+  });
+
+  describe('Given the current user owns the submission', () => {
+    beforeEach(() => {
+      ({ queryByTestId } = renderSubmissionComponent(defaultSubmission));
+    });
+
+    afterAll(() => {
+      jest.resetModules();
+    });
+
+    test('Renders edit button', () => {
+      const editButton = queryByTestId('edit-submission-btn');
+      expect(editButton).toBeInTheDocument();
+    });
+
+    test('Displays edit modal when edit button is clicked.', () => {
+      fireEvent.mouseDown(queryByTestId('edit-submission-btn'));
+
+      const editForm = queryByTestId('edit-form');
+      expect(editForm).toBeInTheDocument();
+    });
+
+    test("Doesn't render report button", () => {
+      const reportButton = queryByTestId('flag-btn');
+      expect(reportButton).not.toBeInTheDocument();
+    });
+  });
+
+  describe('Given the current user does not own the submission', () => {
+    const onFlag = jest.fn();
+
+    beforeEach(() => {
+      ({ queryByTestId } = renderSubmissionComponent(defaultSubmission, 2, onFlag));
+    });
+
+    afterAll(() => {
+      jest.resetModules();
+    });
+
+    test('Does not render edit button', () => {
+      const editButton = queryByTestId('edit-submission-btn');
+      expect(editButton).not.toBeInTheDocument();
+    });
+
+    test('Renders report button', () => {
+      const reportButton = queryByTestId('flag-btn');
+      expect(reportButton).toBeInTheDocument();
+    });
+
+    test('Report button fires onFlag handler', () => {
+      fireEvent.click(queryByTestId('flag-btn'));
+
+      expect(onFlag).toHaveBeenCalledTimes(1);
+    });
+  });
+});
