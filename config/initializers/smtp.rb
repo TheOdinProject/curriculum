1040656162d75c6e034d82b1b026edb95f6b1f90
Ranko Liang <rankoliang@gmail.com>
2021-04-19 21:17:25 -0400

    Set smtp settings after Rails is initialized
    The helpers from ActionText get loaded in too early when ActionMailer is
    initialized early, leading to deprecation warnings. This is a workaround
    to remove the deprecation warnings. This is an issue in the main rails
    repo, but the fix has not been shipped to rubygems yet.
    
    When rails is bumped to a version with this fix, this change should be
    reverted.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '587',
-  authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: 'heroku.com',
-  enable_starttls_auto: true
-}
+Rails.application.config.after_initialize do
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+end
