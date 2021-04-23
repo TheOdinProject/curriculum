14e5b19bf0b8922d4345d297f7a03e83b6a14dd6
Rachel Moser <moser.rachel@gmail.com>
2021-04-23 09:34:17 -0500

    Chore: System Tests for Lesson Completion
    Because
    * Lesson Completion needs end to end test coverage.
    
    This commit:
    * Tests lesson completion and incompletion on lesson page.
    * Tests lesson completion and incompletion on course page.

--- spec/system/course_lesson_completion_spec.rb
+++ spec/system/course_lesson_completion_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Lesson Completions', type: :system do
+RSpec.describe 'Course Lesson Completions', type: :system do
   let!(:path) { create(:path, default_path: true) }
   let!(:course) { create(:course, path: path) }
   let!(:section) { create(:section, course: course) }
@@ -10,28 +10,28 @@ RSpec.describe 'Lesson Completions', type: :system do
 
     before do
       sign_in(create(:user))
-      visit path_course_lesson_path(path, course, lesson)
+      visit path_course_path(path, course)
     end
-    
+
     it 'can complete a lesson' do
-      find(:test_id, 'complete_btn').click
+      find(:test_id, 'lesson_complete_btn').click
 
-      expect(find(:test_id, 'incomplete_btn')).not_to be(nil)
+      expect(find(:test_id, 'lesson_incomplete_btn')).not_to be(nil)
     end
-    
-    it 'can change a completed lesson to incomplete' do
-      find(:test_id, 'complete_btn').click
-      find(:test_id, 'incomplete_btn').click
 
-      expect(find(:test_id, 'complete_btn')).not_to be(nil)
+    it 'can change a completed lesson to incomplete' do
+      find(:test_id, 'lesson_complete_btn').click
+      find(:test_id, 'lesson_incomplete_btn').click
+
+      expect(find(:test_id, 'lesson_complete_btn')).not_to be(nil)
     end
   end
 
   context 'when user is not signed in' do
     it 'cannot complete a lesson' do
-      visit path_course_lesson_path(path, course, lesson)
+      visit path_course_path(path, course)
 
-      expect(find(:test_id, 'login_button')).to have_content('Login to track progress')
+      expect(page).to have_no_button('.section-lessons__item__icon')
     end
   end
 end
