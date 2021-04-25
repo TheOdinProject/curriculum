3c3fa302752b91d922bdafa137a28910c8d2419c
Austin Mason <austinpmason@gmail.com>
2021-04-25 22:01:22 +0100

    Switching path system test cleanup
    Makes the following changes
    - Makes the button test-id names more explicit about what they are.
    - removes unnecessary check for the wording on the button for the system
      test for switching paths on a path show page.
    - checks that the user path was actually changed in the db

--- spec/system/switching_path_spec.rb
+++ spec/system/switching_path_spec.rb
@@ -2,36 +2,36 @@ require 'rails_helper'
 
 RSpec.describe 'Switching Paths', type: :system do
   let!(:default_path) { create(:path, title: 'Foundations', default_path: true) }
-  let!(:unselected_path) { create(:path, title: 'Rails') }
+  let!(:rails_path) { create(:path, title: 'Rails') }
+  let!(:user) { create(:user, path: default_path) }
 
   before do
-    sign_in(create(:user, path: default_path))
+    sign_in(user)
   end
 
   context 'on the paths index page' do
     it 'allows a user to switch paths' do
       visit paths_path
 
-      expect(find(:test_id, 'Foundations').value).to eq('Resume')
-      expect(find(:test_id, 'Rails').value).to eq('Switch')
+      expect(find(:test_id, 'foundations-resume-path-btn').value).to eq('Resume')
+      expect(find(:test_id, 'rails-switch-path-btn').value).to eq('Switch')
 
-      find(:test_id, 'Rails').click
+      find(:test_id, 'rails-switch-path-btn').click
 
       expect(page).to have_css '.alert-success'
       expect(find(:test_id, 'flash')).to have_text 'You have switched to the Rails path'
+      expect(user.reload.path).to eq rails_path
     end
   end
 
   context 'on the path show page' do
     it 'allows a user to switch to that path' do
-      visit path_path(unselected_path)
-
-      expect(find(:test_id, 'Rails').value).to eq('Switch Path')
-
-      find(:test_id, 'Rails').click
+      visit path_path(rails_path)
+      find(:test_id, 'rails-switch-path-btn').click
 
       expect(page).to have_css '.alert-success'
       expect(find(:test_id, 'flash')).to have_text 'You have switched to the Rails path'
+      expect(user.reload.path).to eq rails_path
     end
   end
 end
