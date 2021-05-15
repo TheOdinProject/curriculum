c1f8d737c2f46c4eca2b50ba9d8ffe752ec555ec
Kevin Mulhern <kevinmulhern23@gmail.com>
2021-05-15 16:33:42 +0100

    Chore: Only Report Errors Originating on TheOdinProject
    Because:
    * We are seeing quite a few errors from extentions and external scripts reported.
    
    This commit:
    * Configures the sentry allowed urls list.

--- app/javascript/packs/error_tracking.js
+++ app/javascript/packs/error_tracking.js
@@ -4,7 +4,13 @@ const {
   SENTRY_DSN, currentUserLoggedIn, currentUserId, currentUserEmail,
 } = window;
 
-Sentry.init({ dsn: SENTRY_DSN, environment: 'production' });
+Sentry.init({
+  dsn: SENTRY_DSN,
+  environment: 'production',
+  allowUrls: [
+    /https?:\/\/(www\.)?theodinproject\.com/,
+  ],
+});
 
 Sentry.configureScope((scope) => {
   if (currentUserLoggedIn) {
