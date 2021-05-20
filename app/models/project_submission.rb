c190467a14695a955353b410744cd6245f12d276
Kevin Mulhern <kevinmulhern23@gmail.com>
2021-05-20 02:56:48 +0100

    Fix: Broken Admin Flag Links For Removed Submissions
    Because:
    * The default scope for returning kept project submissions was causing the flags project submission association to return nil after it is soft deleted.
    
    This commit:
    * Removes the default kept scope from project submissions.
    * Adds a discarded_at nil condition to the viewable project submissions scope.
    * Uses the viewable scope when finding the users submission for a project.

--- app/models/project_submission.rb
+++ app/models/project_submission.rb
@@ -1,9 +1,7 @@
 class ProjectSubmission < ApplicationRecord
   include Discard::Model
-  default_scope -> { kept }
 
   acts_as_votable
-
   paginates_per 15
 
   belongs_to :user
@@ -14,6 +12,6 @@ class ProjectSubmission < ApplicationRecord
   validates :live_preview_url, allow_blank: true, url: true
   validates :repo_url, presence: { message: 'Required' }
 
-  scope :viewable, -> { where(is_public: true, banned: false) }
+  scope :viewable, -> { where(is_public: true, banned: false, discarded_at: nil) }
   scope :created_today, -> { where('created_at >= ?', Time.zone.now.beginning_of_day) }
 end
