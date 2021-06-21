9996f20fce8290ff5684a73d662f47a538b4e142
Alex Younger <alexanderyounger@gmail.com>
2021-06-20 20:01:04 -0400

    remove unneeded test, combine tests related to empty submissions array

--- app/javascript/components/project-submissions/components/__tests__/submissions-list.test.jsx
+++ app/javascript/components/project-submissions/components/__tests__/submissions-list.test.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import {
-  fireEvent, render, screen, act,
-} from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 
 import ProjectSubmissionContext from '../../ProjectSubmissionContext';
 import SubmissionsList from '../submissions-list';
@@ -48,57 +46,7 @@ describe('submissions list', () => {
       expect(screen.queryAllByTestId('submission')[2].textContent).toBe('baz');
     });
 
-    it('reorders submissions when a submission gains more likes than the one above it', () => {
-      // this test component is used to mock the user interaction of increasing the likes
-      // on a particular submission
-      const TestComponent = () => {
-        // setup dummy submissions to be able to change submission like numbers with an event
-        const [testSubmissions, setTestSubmissions] = useState(submissions);
-
-        // set 'baz' likes to 10, putting it first in the order instead of third
-        const reorderSubmissions = () => {
-          const newSubmissions = [...submissions];
-          const baz = newSubmissions.findIndex((sub) => sub.id === 'baz');
-          newSubmissions[baz] = { ...newSubmissions[baz], likes: 10 };
-
-          setTestSubmissions(newSubmissions);
-        };
-
-        return (
-          <>
-            <SubmissionsList
-              submissions={testSubmissions}
-              handleDelete={handleDelete}
-              handleUpdate={handleUpdate}
-              handleLikeToggle={handleLikeToggle}
-            />
-            <button
-              type="button"
-              onClick={reorderSubmissions}
-              data-test-id="reorder"
-            >
-              Click me
-            </button>
-          </>
-        );
-      };
-
-      render(
-        <ProjectSubmissionContext.Provider value={{ allSubmissionsPath: '#' }}>
-          <TestComponent />
-        </ProjectSubmissionContext.Provider>,
-      );
-
-      expect(screen.queryAllByTestId('submission')[0].textContent).toBe('foo');
-      expect(screen.queryAllByTestId('submission')[2].textContent).toBe('baz');
-
-      act(() => fireEvent.click(screen.getByTestId('reorder')));
-
-      expect(screen.queryAllByTestId('submission')[0].textContent).toBe('baz');
-      expect(screen.queryAllByTestId('submission')[1].textContent).toBe('foo');
-    });
-
-    it('does not render any submissions when array is empty', () => {
+    it('does not render any submissions when array is empty, an instead renders a no submissions message', () => {
       render(
         <ProjectSubmissionContext.Provider value={{ allSubmissionsPath: '#' }}>
           <SubmissionsList
@@ -111,20 +59,6 @@ describe('submissions list', () => {
       );
 
       expect(screen.queryAllByTestId('submission').length).toBe(0);
-    });
-
-    it('renders a no submissions yet message when array is empty', () => {
-      render(
-        <ProjectSubmissionContext.Provider value={{ allSubmissionsPath: '#' }}>
-          <SubmissionsList
-            submissions={[]}
-            handleDelete={handleDelete}
-            handleUpdate={handleUpdate}
-            handleLikeToggle={handleLikeToggle}
-          />
-        </ProjectSubmissionContext.Provider>,
-      );
-
       expect(
         screen.getByText('No Submissions yet, be the first!'),
       ).toBeInTheDocument();
