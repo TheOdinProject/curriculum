0ce5a038a86bebfb5f21e6734ed12867ceb31f87
Tatiana <hello@tatianacodes.com>
2021-08-07 10:28:03 -0700

    Update maintainer_users.rb

--- db/seeds/maintainer_users.rb
+++ db/seeds/maintainer_users.rb
@@ -1,5 +1,5 @@
 if Rails.env.development? || ENV['STAGING']
-  names = %w[kevin rachel austin sully]
+  names = %w[kevin rachel austin sully tatiana]
 
   names.each do |name|
     User.find_or_create_by!(email: "#{name}@odin.com") do |user|
