6012f5ec2072f66abf1d668593cbdb5b4881b29d
CatQueenCodes <59713017+CatQueenCodes@users.noreply.github.com>
2021-08-14 10:59:47 -0400

    Update maintainer_users.rb
    Added self

--- db/seeds/maintainer_users.rb
+++ db/seeds/maintainer_users.rb
@@ -1,5 +1,5 @@
 if Rails.env.development? || ENV['STAGING']
-  names = %w[kevin rachel austin sully tatiana briggs leo]
+  names = %w[kevin rachel austin sully tatiana briggs leo grace]
 
   names.each do |name|
     User.find_or_create_by!(email: "#{name}@odin.com") do |user|
