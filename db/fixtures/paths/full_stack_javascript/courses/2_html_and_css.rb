6250f226d515c5434fa5e3e56b7bb00e284dff0b
Cody Loyd <codyloyd@gmail.com>
2021-09-03 11:27:33 -0500

    add lessons

--- db/fixtures/paths/full_stack_javascript/courses/2_html_and_css.rb
+++ db/fixtures/paths/full_stack_javascript/courses/2_html_and_css.rb
@@ -63,6 +63,25 @@ course.add_section do |section|
 end
 
 # +++++++++++++++++++++++
+# SECTION - Accessibility
+# +++++++++++++++++++++++
+course.add_section do |section|
+  section.title = 'Accessibility'
+  section.description = 'Learn how to not get sued for having a shitty website'
+  section.identifier_uuid = '5867ba24-9322-4219-93dd-eaeb05017349'
+
+  section.add_lessons(
+    html_and_css_lessons.fetch('Introduction to Web Accessibility')
+    html_and_css_lessons.fetch('The Web Content Accessibility Guidelines')
+    html_and_css_lessons.fetch('Semantic HTML')
+    html_and_css_lessons.fetch('Accessible Colors')
+    html_and_css_lessons.fetch('Keyboard Navigation')
+    html_and_css_lessons.fetch('Meaningful Text')
+    html_and_css_lessons.fetch('WAI-ARIA')
+    html_and_css_lessons.fetch('Accessibility Auditing')
+  )
+
+# +++++++++++++++++++++++
 # SECTION - Design and UX
 # +++++++++++++++++++++++
 course.add_section do |section|
