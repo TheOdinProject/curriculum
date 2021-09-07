718142fd4faa68b1656d5b34b79d8423155a385f
Cody Loyd <codyloyd@gmail.com>
2021-09-07 12:38:38 -0500

    update titles

--- db/fixtures/paths/foundations/seed.rb
+++ db/fixtures/paths/foundations/seed.rb
@@ -88,8 +88,8 @@ end
 # SECTION - HTML
 # +++++++++++++++++++++++++++++++
 course.add_section do |section|
-  section.title = 'HTML and CSS (New Lessons!)'
-  section.description = 'This and the following sections are new material! Learn the foundations of HTML, a fundamental building block of everything on the web.'
+  section.title = 'HTML Foundations (New Lessons!)'
+  section.description = 'Learn the foundations of HTML, a fundamental building block of everything on the web.'
   section.identifier_uuid = '8b6abb7a-095f-4c1d-b81f-e348aaaf2894'
 
   section.add_lessons(
@@ -108,7 +108,7 @@ end
 # +++++++++++++++++++++++++++++++
 course.add_section do |section|
   section.title = 'CSS Foundations (New Lessons!)'
-  section.description = 'Learn how to make your HTML look they way you want by adding styles with CSS.'
+  section.description = 'Learn how to make your HTML look the way you want by adding styles with CSS.'
   section.identifier_uuid = 'd2476929-d71b-4b25-969b-8f7da6a40c94'
 
   section.add_lessons(
