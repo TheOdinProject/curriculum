03753172baaa5b735c4299198f8b33e283eda13c
Kevin Mulhern <kevinmulhern23@gmail.com>
2021-09-28 23:50:06 +0100

    Fix: Seg Fault Error with Sassc Gem
    Because:
    * It is making our CI runs flaky.
    
    This commit:
    * Removes the assets precompilation of the application_stylesheet since it is no longer supported and should already be done in the assets/manifest.js file.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
+# work around for segfault error with sassc that is appearing in CI for us atm - https://github.com/sass/sassc-ruby/issues/197
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
@@ -7,8 +12,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-Rails.application.config.assets.precompile += %w[application_stylesheet.scss]
+Rails.application.config.assets.paths << Rails.root.join('app/assets/fonts')
