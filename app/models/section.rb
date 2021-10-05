ec3e1a1e0ab5790ac236e3c90d22e4b4c5fcdb95
Kevin Mulhern <kevinmulhern23@gmail.com>
2021-10-05 20:16:11 +0100

    Fix: linting

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ApplicationRecord
   belongs_to :course
-  has_many :lessons, -> { order(:position) }, inverse_of: :section
+  has_many :lessons, -> { order(:position) }, inverse_of: :section, dependent: :destroy
 
   validates :position, presence: true
 end
