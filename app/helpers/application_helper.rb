b1a086614d219a0a1686065c601e21eec50ed2ce
Austin <austinpmason@gmail.com>
2021-10-11 11:42:54 +0100

    Revert "Revert "fix: change svg inline rendering method to use a document fragment""
    This reverts commit 21eca0895620788302a85a75b4705801a78e1c18.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Metrics/MethodLength, Layout/LineLength, Metrics/ModuleLength, Rails/OutputSafety
+# rubocop:disable Metrics/MethodLength, Layout/LineLength, Metrics/ModuleLength
 module ApplicationHelper
   require 'kramdown'
 
@@ -224,14 +224,6 @@ module ApplicationHelper
     end
   end
 
-  def svg_icon(filename)
-    file_path = Rails.root.join('app', 'assets', 'images', 'icons', "#{filename}.svg")
-
-    if File.exist?(file_path)
-      File.read(file_path).html_safe
-    end
-  end
-
   def unread_notifications?(user)
     user.notifications.any?(&:unread?)
   end
@@ -250,4 +242,4 @@ module ApplicationHelper
     'https://medium.com/the-odin-project'
   end
 end
-# rubocop:enable Metrics/MethodLength, Layout/LineLength, Metrics/ModuleLength, Rails/OutputSafety
+# rubocop:enable Metrics/MethodLength, Layout/LineLength, Metrics/ModuleLength
