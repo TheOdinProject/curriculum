951b5ec144f5bfc978a5bb345b32240e50f3e8c1
Cody Loyd <codyloyd@gmail.com>
2022-01-03 13:47:56 -0600

    add lessons to rails course

--- db/fixtures/paths/full_stack_rails/courses/4_html_and_css.rb
+++ db/fixtures/paths/full_stack_rails/courses/4_html_and_css.rb
@@ -3,62 +3,81 @@
 ########################
 course = @path.add_course do |course|
   course.title = 'HTML and CSS'
-  course.description = "Good web design doesn't happen by accident. Now that you've gained a strong foundation in programming, this course takes a deeper look at front-end design, expanding on what is covered in Foundations. You'll learn how to design websites that look great on any device and you'll be equipped to deeply understand and create your own responsive design framework!"
-  course.identifier_uuid = '9f2e358a-50ad-42ea-8f64-8d1e613d74ce'
+  course.description = "It's time to dig in and become the CSS expert you deserve to be. After this course you'll be equipped to create web projects that look beautiful on any device!"
+  course.identifier_uuid = '70434eab-0ff2-4db5-9a4c-b78cebd010fe'
 end
 
 # +++++++++++++++++++++++++++++++++++
-# SECTION - Basic HTML Page Structure
+# SECTION - Intermediate HTML Concepts
 # +++++++++++++++++++++++++++++++++++
 course.add_section do |section|
-  section.title = 'Basic HTML Page Structure'
-  section.description = "In this section, we'll cover the whole range of HTML so you'll be completely comfortable with putting the right elements in the right places on a page."
-  section.identifier_uuid = 'dc04b57f-9e90-43f7-b8f4-a6598e10ea90'
+  section.title = 'Intermediate HTML Concepts'
+  section.description = "In this section, we'll finish covering the whole range of HTML so you'll be completely comfortable with putting the right elements in the right places on a page."
+  section.identifier_uuid = '5388096b-4926-47a7-8891-a1dfa53cf200'
 
   section.add_lessons(
-    html_and_css_lessons.fetch('How This Course Will Work'),
-    html_and_css_lessons.fetch('HTML Basics'),
-    html_and_css_lessons.fetch('Linking Internal and External Pages'),
-    html_and_css_lessons.fetch('Working with Images, Video and Other Media'),
-    html_and_css_lessons.fetch('Embedding Images and Video'),
-    html_and_css_lessons.fetch("What's New in HTML5")
+    html_and_css_lessons.fetch('Introduction'),
+    html_and_css_lessons.fetch('Emmet'),
+    html_and_css_lessons.fetch('Form Basics'),
+    html_and_css_lessons.fetch('Form Validation'),
+    html_and_css_lessons.fetch('SVG'),
+    html_and_css_lessons.fetch('Tables'),
   )
 end
 
 # +++++++++++++++++++++++++++++++++++++++
-# SECTION - Displaying and Inputting Data
+# SECTION - Intermediate CSS Concepts
 # +++++++++++++++++++++++++++++++++++++++
 course.add_section do |section|
-  section.title = 'Displaying and Inputting Data'
-  section.description = "Displaying and inputting data are two of your chief duties as a web developer. We'll cover the tools at your disposal, including tables and lists for display and forms for input."
-  section.identifier_uuid = '79ed6ec0-7711-48c0-bf75-920192ab432f'
+  section.title = 'Intermediate CSS Concepts'
+  section.description = "Our Foundations course taught you the basics of CSS, but here we'll really dive into what you need to know to make your web projects look however you want."
+  section.identifier_uuid = '92d15409-837a-44cb-9183-6a5fa5b7c5c2'
 
   section.add_lessons(
-    html_and_css_lessons.fetch('Tables in HTML'),
-    html_and_css_lessons.fetch('Ordered and Unordered Lists'),
-    html_and_css_lessons.fetch('Forms for Collecting Data'),
-    html_and_css_lessons.fetch('HTML Forms'),
+    html_and_css_lessons.fetch('Default Styles'),
+    html_and_css_lessons.fetch('CSS Units'),
+    html_and_css_lessons.fetch('More Text Styles'),
+    html_and_css_lessons.fetch('More CSS Properties'),
+    html_and_css_lessons.fetch('Advanced Selectors'),
+    html_and_css_lessons.fetch('Positioning'),
+    html_and_css_lessons.fetch('CSS Functions'),
+    html_and_css_lessons.fetch('Custom Properties'),
+    html_and_css_lessons.fetch('Browser Compatibility'),
+    html_and_css_lessons.fetch('Frameworks and Preprocessors'),
+    html_and_css_lessons.fetch('Project: Sign-up Form'),
   )
 end
 
 # +++++++++++++
-# SECTION - CSS
+# SECTION - ANIMATION
 # +++++++++++++
 course.add_section do |section|
-  section.title = 'CSS'
-  section.description = "Here we'll cover each of the foundational CSS concepts in greater depth than you probably have before."
-  section.identifier_uuid = 'ac4174f2-c2a4-4e11-be00-52d8abfaaf1d'
+  section.title = 'Animation'
+  section.description = "Let's learn how to make things MOVE."
+  section.identifier_uuid = '7fb7fd1a-3817-49e0-95d0-b46ecf1d5d7f'
 
   section.add_lessons(
-    html_and_css_lessons.fetch('CSS Basics'),
-    html_and_css_lessons.fetch('The Box Model'),
-    html_and_css_lessons.fetch('Floats and Positioning'),
-    html_and_css_lessons.fetch('Flexbox'),
-    html_and_css_lessons.fetch('Grid'),
-    html_and_css_lessons.fetch('Positioning and Floating Elements'),
-    html_and_css_lessons.fetch('Best Practices'),
-    html_and_css_lessons.fetch('Backgrounds and Gradients'),
-    html_and_css_lessons.fetch('Building with Backgrounds and Gradients'),
+    html_and_css_lessons.fetch('Transforms'),
+    html_and_css_lessons.fetch('Transitions'),
+    html_and_css_lessons.fetch('Keyframes'),
+  )
+end
+
+# +++++++++++++
+# SECTION - GRID
+# +++++++++++++
+course.add_section do |section|
+  section.title = 'Grid'
+  section.description = "Learn how to use the new CSS Grid syntax to build your layouts"
+  section.identifier_uuid = 'd4614f98-b9d5-494f-b551-8842d4aa73f9'
+
+  section.add_lessons(
+    html_and_css_lessons.fetch('Introduction to Grid'),
+    html_and_css_lessons.fetch('Creating a Grid'),
+    html_and_css_lessons.fetch('Positioning Grid Elements'),
+    html_and_css_lessons.fetch('Advanced Grid Properties'),
+    html_and_css_lessons.fetch('Project: Grid'),
+    html_and_css_lessons.fetch('Grid vs. Flex'),
   )
 end
 
@@ -68,7 +87,7 @@ end
 course.add_section do |section|
   section.title = 'Accessibility'
   section.description = "The websites you create will be viewed by a wide range of users, including those who rely on various accessibility features due to any number of disabilities or limitations. As important as it is to make your site look good, it's just as important to make it usable by as many different users as possible."
-  section.identifier_uuid = '5867ba24-9322-4219-93dd-eaeb05017349'
+  section.identifier_uuid = 'c4200ae6-e97d-4e7a-9cd6-18dfd443953e'
 
   section.add_lessons(
     html_and_css_lessons.fetch('Introduction to Web Accessibility'),
@@ -82,49 +101,19 @@ course.add_section do |section|
   )
 end
 
-# +++++++++++++++++++++++
-# SECTION - Design and UX
-# +++++++++++++++++++++++
-course.add_section do |section|
-  section.title = 'Design and UX'
-  section.description = "If you want to make your websites stop looking like they came from the 1990's, you'll need to gain an understanding for at least the best practices of design and User Experience (UX).  Not knowing this stuff is like charging over the next hill without any idea of why you're doing it."
-  section.identifier_uuid = '721dbec7-fb68-4c28-aad9-044ae6d0c032'
-
-  section.add_lessons(
-    html_and_css_lessons.fetch('Introduction to Design and UX'),
-    html_and_css_lessons.fetch('Fonts and Typography'),
-    html_and_css_lessons.fetch('Design Teardown'),
-  )
-end
-
 # ++++++++++++++++++++++++++++++++++++++++++++++
 # SECTION - Responsive Design and CSS Frameworks
 # ++++++++++++++++++++++++++++++++++++++++++++++
 course.add_section do |section|
-  section.title = 'Responsive Design and CSS Frameworks'
-  section.description = 'These days you need to make sure your pages display easily on multiple viewport sizes by using fluid layouts and media queries. Luckily there are CSS frameworks like Twitter Bootstrap that can save you a ton of time developing standard pages and which come with responsive functionality for free.'
-  section.identifier_uuid = 'b99f6f56-1f50-4ba2-8a8a-f2e08bee0499'
+  section.title = 'Responsive Design'
+  section.description = 'These days you need to make sure your pages display easily on multiple viewport sizes by using fluid layouts and media queries.'
+  section.identifier_uuid = '556d8b4d-6f91-469f-b8eb-288e8a7d7642'
 
   section.add_lessons(
-    html_and_css_lessons.fetch('Responsive Design'),
-    html_and_css_lessons.fetch('Building with Responsive Design'),
-    html_and_css_lessons.fetch('CSS Frameworks like Bootstrap and Foundation'),
-  )
-end
-
-# ++++++++++++++++++++++
-# SECTION - Advanced CSS
-# ++++++++++++++++++++++
-course.add_section do |section|
-  section.title = 'Advanced CSS'
-  section.description = "We'll take you beyond the basics of CSS and into a variety of additional topics from how to add some stylistic flair to your elements to using tools like preprocessors to save time and reduce repetition in your code."
-  section.identifier_uuid = '31c08007-c670-4a7d-bb89-fa9c0a06f539'
-
-  section.add_lessons(
-    html_and_css_lessons.fetch('Animations, Subtle Effects and Compatibility'),
-    html_and_css_lessons.fetch('Using CSS Preprocessors to Save Time'),
-    html_and_css_lessons.fetch('Design Your Own Grid-Based Framework'),
-    html_and_css_lessons.fetch('Conclusion'),
+    html_and_css_lessons.fetch('Introduction to Responsive Design'),
+    html_and_css_lessons.fetch('Natural Responsiveness'),
+    html_and_css_lessons.fetch('Responsive Images'),
+    html_and_css_lessons.fetch('Media Queries'),
   )
 end
 
