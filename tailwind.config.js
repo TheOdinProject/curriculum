702758d719c6a0f6d13c22937db3a35fe3d2618e
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-01-14 00:24:25 +0000

    Feature: Enable Tailwind Base Directive
    Because:
    * We need this enabled because borders do not work without it. However it includes the Tailwind Preflight base styles which is effectivley a css reset. This has affected our headings and some images, therefore those needed to be moved over to Tailwind styles as part of this PR.
    
    This commit:
    * Enables Tailwind base directive.
    * Fixes the headings affected by the css reset.
    * Fixes the images affected by the css reset.
    * Adds the odin gold brand color to Tailwinds color config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'gold': {
+          DEFAULT: '#CE973E',
+          '50': '#F3E6D0',
+          '100': '#EFDDC0',
+          '200': '#E7CCA0',
+          '300': '#DFBA7F',
+          '400': '#D6A95F',
+          '500': '#CE973E',
+          '600': '#A9792B',
+          '700': '#7C5920',
+          '800': '#503914',
+          '900': '#231909'
+        },
         'nav-link-read': 'rgba(74, 74, 74, 0.7)',
         'nav-link-unread': 'rgba(206, 151, 62, 0.7)',
         'notification': 'rgba(74, 74, 74, 0.7)',
