cc9eb8ebd4f67bc013caf8edbe132d22cf16771d
Tatiana <hello@tatianacodes.com>
2022-01-15 08:26:28 -0800

    Modularize function

--- app/javascript/src/js/darkModeBeta.js
+++ app/javascript/src/js/darkModeBeta.js
@@ -1,22 +1,61 @@
 function enableDarkMode() {
     const navbar = document.querySelector('.navbar-nav');
-    const darkModeItem = document.createRange().createContextualFragment(
-        `<li class="nav-item " style="flex-direction: row; justify-content: center">
-          <a class="nav-link" data-test-id="user-login-link">Dark Mode</a>
-          <input type="checkbox" style="align-self: center">
-        </li>`);
+    const userSelection = checkStorage()
+    const darkModeItem = createIcon(userSelection)
 
-    darkModeItem.querySelector("input").addEventListener('click', async e => {
-        if(e.target.checked) {
-            const result = await fetch("https://cdn.jsdelivr.net/gh/TheOdinProject/top-dark-theme@master/darkMode.css");
-            const css = await result.text();
-            const style = document.createElement('style');
-            style.innerText = css;
-            document.querySelector('head').appendChild(style);
-        } else {
-            console.log('unchecked');
-        }
+    darkModeItem.querySelector("button").addEventListener('click', async e => {
+      const result = await fetch("https://cdn.jsdelivr.net/gh/TheOdinProject/top-dark-theme@master/darkMode.css");
+      const css = await result.text();
+      const style = document.createElement('style');
+      style.innerText = css;
+      document.querySelector('head').appendChild(style);
     });
 
     navbar.appendChild(darkModeItem);
 }
+// 
+
+function checkStorage(){
+  const darkModeOption = localStorage.getItem('topDarkMode')
+  // If it doesn't exist, set to false
+  if(darkModeOption === null){
+    localStorage.setItem('topDarkMode', false)
+    return false
+  } else {
+    // If it exists, return user's selection
+    return darkModeOption
+  }
+}
+
+function toggleOption(){
+  const currentSelection = localStorage.getItem('topDarkMode')
+  localStorage.setItem('topDarkMode', !currentSelection)
+}
+
+function createIcon(darkModeEnabled){
+  let darkModeButton;
+  if(darkModeEnabled){
+    // Sun icon
+    darkModeButton = document.createRange().createContextualFragment(
+      `<li class="nav-item " style="flex-direction: row; justify-content: center">
+        <button data-test-id="dark-mode-button">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </button>
+      </li>`);   
+  } else {
+    // Moon icon
+    darkModeButton = document.createRange().createContextualFragment(
+      `<li class="nav-item " style="flex-direction: row; justify-content: center">
+        <button data-test-id="dark-mode-button">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+          </svg>
+        </button>
+      </li>`);
+  }
+  return darkModeButton
+}
+
+enableDarkMode()
