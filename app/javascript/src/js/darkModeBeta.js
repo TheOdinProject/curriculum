1ed95c79a175b1e4572fccc2996104bafa390f12
Tatiana <hello@tatianacodes.com>
2022-01-15 09:56:45 -0800

    Fix conditional logic

--- app/javascript/src/js/darkModeBeta.js
+++ app/javascript/src/js/darkModeBeta.js
@@ -24,7 +24,6 @@ async function toggleOption() {
   const isDarkModeEnabled = checkStorage();
   if (isDarkModeEnabled) {
     document.removeChild(document.querySelector('#darkModeStyles'));
-    localStorage.setItem('topDarkMode', !isDarkModeEnabled);
   } else {
     const result = await fetch('https://cdn.jsdelivr.net/gh/TheOdinProject/top-dark-theme@master/darkMode.css');
     const css = await result.text();
@@ -33,6 +32,8 @@ async function toggleOption() {
     style.id = 'darkModeStyles';
     document.querySelector('head').appendChild(style);
   }
+  localStorage.setItem('topDarkMode', JSON.stringify(!isDarkModeEnabled));
+  swapIcons(!isDarkModeEnabled);
 }
 
 function createIcon(darkModeEnabled) {
@@ -40,10 +41,10 @@ function createIcon(darkModeEnabled) {
   if (darkModeEnabled) {
     // Sun icon
     darkModeButton = document.createRange().createContextualFragment(
-      `<li class="nav-item " style="flex-direction: row; justify-content: center">
+      `<li class="nav-item" id="dark-mode-item" style="flex-direction: row; justify-content: center">
         <button data-test-id="dark-mode-button">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
           </svg>
         </button>
       </li>`,
@@ -51,7 +52,7 @@ function createIcon(darkModeEnabled) {
   } else {
     // Moon icon
     darkModeButton = document.createRange().createContextualFragment(
-      `<li class="nav-item " style="flex-direction: row; justify-content: center">
+      `<li class="nav-item" id="dark-mode-item" style="flex-direction: row; justify-content: center">
         <button data-test-id="dark-mode-button">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
@@ -61,6 +62,14 @@ function createIcon(darkModeEnabled) {
     );
   }
   return darkModeButton;
+}
+
+function swapIcons(isDarkModeEnabled) {
+  const currentButton = document.querySelector('#dark-mode-item');
+  const newButton = createIcon(isDarkModeEnabled);
+  const navbar = document.querySelector('.navbar-nav');
+  navbar.removeChild(currentButton);
+  navbar.appendChild(newButton);
 }
 
 document.addEventListener('DOMContentLoaded', () => enableDarkMode());
