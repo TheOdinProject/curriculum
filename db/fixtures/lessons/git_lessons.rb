8ef3869ff54735429ed87449618ec3f4d3323c85
Austin Sullivan <wise.king.sullyman@gmail.com>
2022-03-08 21:41:54 -0500

    Add git lessons fixture file

---
+++ db/fixtures/lessons/git_lessons.rb
@@ -0,0 +1,18 @@
+def git_lessons
+  {
+    'A Deeper Look at Git' => {
+      title: 'A Deeper Look at Git',
+      description: 'Beyond just `$ git add` and `$ git commit`...',
+      is_project: false,
+      url: '/git/lesson_a_deeper_look_at_git.md',
+      identifier_uuid: '52b17564-0e1d-4c4f-adfa-acba53bf9126',
+    },
+    'Using Git in the Real World' => {
+      title: 'Using Git in the Real World',
+      description: "We've just scratched the surface, so here's what to be aware of as you start developing more and more using Git.",
+      is_project: false,
+      url: '/git/lesson_using_git_in_the_real_world.md',
+      identifier_uuid: 'c8b7ccc1-8a16-4545-9d46-b9091d45c6b4',
+    },
+  }
+end
