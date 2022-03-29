6ae9e594c526b4e4d0ae65a09168a425c6d7ef79
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-03-29 22:20:46 +0100

    Chore: Remove Old Logo Styles
    Because:
    * We no longer use these styles.
    
    This commit:
    * Removes the logo css file.
    * Removes the norse font file and references.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,3 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app/assets/fonts')
