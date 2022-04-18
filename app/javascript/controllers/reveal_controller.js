f4ea9b5ee68fd76a088cc362ddaa950e8fac3250
Austin Mason <austinpmason@gmail.com>
2022-04-18 21:21:07 +0100

    Convert FAQ page to tailwind
    Because:
    The site is converting the css to use tailwind
    
    This commit:
    
    * Add chevron down svg
    * Add content-container-lg css class
    * Update reveal_controller to toggle open and close classes on icon
    * delete faq_items helper method
    * delete faq_card partial
    * remove bootstrap div classes
    * inline faq card items into accordion view
    * add system tests for faq click behaviour

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class RevealController extends Controller {
-  static targets = ['content'];
+  static targets = ['content', 'icon'];
+
+  static values = {
+    toggleClasses: Array,
+  };
 
   toggle() {
     this.contentTarget.classList.toggle('hidden');
+    this.toggleClassesValue.forEach((classValue) => this.iconTarget.classList.toggle(classValue));
   }
 }
