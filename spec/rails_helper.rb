aae018e66605d328be5558ee211e7f63ec28fdee
Dylan Williams <dwilliams@camplify.com.au>
2022-04-22 10:59:45 +1000

    Upgrade app from Ruby 2.7 to Ruby 3.0
    * Update Ruby version
    * Automatically bump Bundler major version  from 1 -> 2
    * Update docker image and bundler version in circleci config
    * Fix new Rubocop error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration[6.0].maintain_test_schema!
 
 RSpec.configure do |config|
