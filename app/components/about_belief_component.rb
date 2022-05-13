ea8013a5c8a9fba04f7ecad1ebdd84dc0ac62f79
L. P. Soai-Van <leo.soaivan@gmail.com>
2022-05-12 22:30:34 -0400

    Replace beliefs with ViewComponent

---
+++ app/components/about_belief_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AboutBeliefComponent < ViewComponent::Base
+    with_collection_parameter :belief
+
+    def initialize(belief:)
+      @belief = belief
+    end
+  end
+  
