b79e05f6974e1ef5b0b63c8cee8c3eafbf05fb10
larkin <lwellerwilliams@gmail.com>
2022-05-20 21:44:52 -0700

    Add sort function to installation lessons appendix

--- app/controllers/lessons/installation_lessons_controller.rb
+++ app/controllers/lessons/installation_lessons_controller.rb
@@ -1,7 +1,9 @@
 module Lessons
   class InstallationLessonsController < ApplicationController
     def index
-      @lessons = Lesson.installation_lessons
+      @lessons = Lesson.installation_lessons.sort_by do |lesson|
+        [lesson.course.position, lesson.path.position]
+      end
     end
   end
 end
