5c92b54676533ffc117e918deb92e590bf04bc8f
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-06-06 21:23:11 +0100

    Chore: Move Faq Items to Locale
    Because:
    * It gives reduces the duplication on the faq view.
    
    This commit:
    * Move all faq questions and answers to the en locale file.
    * Allow faq item component to accept an item hash.
    * Change global p tag styling to bring it in line with our newer Tailwind styles.

--- app/components/faq_item_component.rb
+++ app/components/faq_item_component.rb
@@ -3,4 +3,12 @@
 class FaqItemComponent < ViewComponent::Base
   renders_one :question
   renders_one :answer
+
+  def initialize(item:)
+    @item = item
+  end
+
+  private
+
+  attr_reader :item
 end
