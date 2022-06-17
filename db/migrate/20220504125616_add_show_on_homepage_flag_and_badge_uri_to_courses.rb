c6674e7fd5bed5f94b3dc24cbc27ce3475c9273b
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-06-17 16:18:53 +0100

    Chore: Remove null constraint

--- db/migrate/20220504125616_add_show_on_homepage_flag_and_badge_uri_to_courses.rb
+++ db/migrate/20220504125616_add_show_on_homepage_flag_and_badge_uri_to_courses.rb
@@ -1,6 +1,6 @@
 class AddShowOnHomepageFlagAndBadgeUriToCourses < ActiveRecord::Migration[6.1]
   def change
     add_column :courses, :show_on_homepage, :boolean, default: false, null: false
-    add_column :courses, :badge_uri, :string, null: false
+    add_column :courses, :badge_uri, :string
   end
 end
