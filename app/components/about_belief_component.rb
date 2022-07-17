28a068cbd0e817ce9b1209d4d238a2d3ea0316aa
L. P. Soai-Van <leo.soaivan@gmail.com>
2022-07-17 14:57:14 -0400

    Cleanup indentatio n

--- app/components/about_belief_component.rb
+++ app/components/about_belief_component.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class AboutBeliefComponent < ViewComponent::Base
-    with_collection_parameter :belief
+  with_collection_parameter :belief
 
-    def initialize(belief:)
-      @belief = belief
-    end
+  def initialize(belief:)
+    @belief = belief
   end
-  
+end
