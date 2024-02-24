22fd42dc0f1289f77e7c7774a46d9adedbb130b9
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-24 16:05:45 +0000

    fix: asterisk getting replaced in links

--- markdowntool/correctBoldAndItalic.js
+++ markdowntool/correctBoldAndItalic.js
@@ -3,10 +3,12 @@ function boldAndItalic(lines) {
 
   let marker = false;
   lines.forEach((line) => {
-    if (!marker) {
-      if (!line.content.includes("`")) {
-        arrayToExamine.push(line);
-      }
+    if (
+      !line.content.includes("`") &&
+      !marker &&
+      !line.content.includes("http")
+    ) {
+      arrayToExamine.push(line);
     }
     if (line.content.includes("```" && marker)) {
       marker = false;
