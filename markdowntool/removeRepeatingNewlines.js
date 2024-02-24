7f5c46653c324bc5ed29d22d6657b634891e091c
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-24 14:02:51 +0000

    feat: removes repeating newlines from lesson

---
+++ markdowntool/removeRepeatingNewlines.js
@@ -0,0 +1,11 @@
+function removeRepeatingNewlines(lines) {
+  return lines.filter((line, index) => {
+    if (line.content.trim().length === 0) {
+      return index === 0 ? true : line.content !== lines[index - 1].content;
+    } else {
+      return true;
+    }
+  });
+}
+
+export { removeRepeatingNewlines };
