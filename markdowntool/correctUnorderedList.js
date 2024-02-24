2e3fa938b10d190d7895f38b010fc79c32c449fd
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-24 14:01:02 +0000

    feat: script that makes unordered lists align with the style guide

---
+++ markdowntool/correctUnorderedList.js
@@ -0,0 +1,13 @@
+function correctUnorderedList(lines) {
+  lines.forEach((line) => {
+    if (line.content.trim().startsWith("* ")) {
+      const index = line.content.indexOf("*");
+      const splitLine = line.content.split("");
+      splitLine[index] = "-";
+      line.content = splitLine.join("");
+    }
+  });
+  return lines;
+}
+
+export { correctUnorderedList };
