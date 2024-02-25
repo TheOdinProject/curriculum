f3843a6e29de5329e7248f7e69742afc93892646
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-25 13:05:37 +0000

    fix: knowledge check to have proper capitalisation

--- markdowntool/defaultSectionContent.js
+++ markdowntool/defaultSectionContent.js
@@ -1,15 +1,16 @@
-const { data, legacyData } = require("./api");
+const { data } = require("./api");
 const { Line } = require("./line");
 
 function sectionsDefaultContent(lines) {
   Object.entries(data.defaultContent).forEach(([title, description]) => {
     lines.forEach((line, index) => {
-      if (line.content.includes("### Learning Outcomes")) {
+      if (line.content.toLowerCase().includes("### learning outcomes")) {
         line.content = "### Lesson overview";
       }
-      if (
-        line.content.toLowerCase().includes("### ".concat(title.toLowerCase()))
-      ) {
+      if (line.content.toLowerCase().includes('### '.concat(title.toLowerCase()))) {
+        line.content = '### '.concat(title);
+      }
+      if (line.content === '### '.concat(title)) {
         if (
           lines[index + 2].content.trim().startsWith("-") ||
           lines[index + 2].content.trim().startsWith("1")
@@ -17,13 +18,33 @@ function sectionsDefaultContent(lines) {
           lines.splice(index, 0, new Line());
           lines.splice(index, 0, new Line());
           lines[index + 2].content = description;
-          lines[index].content = "### ".concat(title);
         }
         if (lines[index + 2].content !== description) {
           lines[index + 2].content = description;
           lines.splice(index + 3, 0, new Line());
-        }
+        } 
       }
+      // if (
+      //   line.content.toLowerCase().includes("### ".concat(title.toLowerCase()))
+      // ) {
+      //   console.log(line.content);
+      //   if (
+      //     lines[index + 2].content.trim().startsWith("-") ||
+      //     lines[index + 2].content.trim().startsWith("1")
+      //   ) {
+      //     lines.splice(index, 0, new Line());
+      //     lines.splice(index, 0, new Line());
+      //     lines[index + 2].content = description;
+      //     lines[index].content = "### ".concat(title);
+      //   }
+      //   if (lines[index + 2].content !== description) {
+      //     lines[index + 2].content = description;
+      //     lines.splice(index + 3, 0, new Line());
+      //   }
+      // }
+
+
+
     });
   });
   return lines;
