c5e655ed427a65895f9c0a7bab56a71ea3da6236
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-07-09 14:22:00 +0100

    Write note box heading level custom rule
    Add to markdownlint config custom rules array

---
+++ markdownlint/TOP012_noteBoxHeadingLevel/TOP012_noteBoxHeadingLevel.js
@@ -0,0 +1,36 @@
+module.exports = {
+  names: ["TOP012", "note-box-heading-level"],
+  description: "Note box headings must be level 4 (####)",
+  information: new URL(
+    "https://github.com/TheOdinProject/curriculum/blob/main/markdownlint/docs/TOP012.md"
+  ),
+  tags: ["headings"],
+  parser: "markdownit",
+  function: function TOP012(params, onError) {
+    const noteBoxHeadings = params.parsers.markdownit.tokens.filter((token, index, tokens) => {
+      const previousToken = tokens[index - 1];
+
+      const isFollowingNoteBoxOpening =
+        previousToken?.line?.startsWith("<div ") && previousToken.line.includes("lesson-note");
+      return token.type === "heading_open" && isFollowingNoteBoxOpening;
+    });
+
+    noteBoxHeadings.forEach((heading) => {
+      const headingHashes = heading.markup;
+      if (headingHashes.length === 4) {
+        return;
+      }
+
+      const headingText = heading.line.slice(headingHashes.length + 1);
+      onError({
+        lineNumber: heading.lineNumber,
+        detail: `\n  Expected: #### ${headingText}\n  Actual: ${headingHashes} ${headingText}\n`,
+        fixInfo: {
+          editColumn: 1,
+          deleteCount: headingHashes.length,
+          insertText: "####",
+        },
+      });
+    });
+  },
+};
