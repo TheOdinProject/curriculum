55d114fbbe7094e9d63da8dd32da381b486706f4
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-08-02 22:53:25 +0100

    Amend custom rule description
    More enforcing language

--- markdownlint/TOP001_descriptiveLinkText/TOP001_descriptiveLinkText.js
+++ markdownlint/TOP001_descriptiveLinkText/TOP001_descriptiveLinkText.js
@@ -24,7 +24,7 @@ const BLACKLISTED_LINK_TEXT = [
 
 module.exports = {
   names: ["TOP001", "descriptive-link-text"],
-  description: "Links have descriptive text labels",
+  description: "Links must have descriptive text labels",
   tags: ["accessibility", "links"],
   parser: "markdownit",
   information: new URL(
