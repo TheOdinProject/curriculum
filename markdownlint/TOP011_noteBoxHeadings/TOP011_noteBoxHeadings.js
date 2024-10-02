567c26125fd4ebaa4f8501ada8d517bda9c4d920
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-10-02 22:22:31 +0100

    Enforce level 4 headings for all note boxes
    3 different types of note box heading errors now
    - missing heading
    - incorrect heading level
    - incorrect heading start text (type-dependent)
    
    Will help make note boxes more accessible (linkable via heading id
    fragment, and note box type not limited to visual differences and div class).

--- markdownlint/TOP011_noteBoxHeadings/TOP011_noteBoxHeadings.js
+++ markdownlint/TOP011_noteBoxHeadings/TOP011_noteBoxHeadings.js
@@ -1,3 +1,42 @@
+function capitalize(text) {
+  return `${text[0].toUpperCase()}${text.substring(1)}`;
+}
+
+function isNoteBoxOpenTag(token) {
+  return token?.type === "html_block" && token?.content.includes("lesson-note");
+}
+
+function lacksHeading(token, index, tokens) {
+  return isNoteBoxOpenTag(token) && tokens[index + 1]?.type !== "heading_open";
+}
+
+function categoriseHeadingsByNoteBoxType(
+  headings,
+  currentToken,
+  index,
+  tokens
+) {
+  const previousToken = tokens[index - 1];
+  if (
+    currentToken.type !== "heading_open" ||
+    !isNoteBoxOpenTag(previousToken)
+  ) {
+    return headings;
+  }
+
+  // https://regexr.com/86jmp to test the regex below
+  const noteBoxType = capitalize(
+    previousToken.content.match(/(?<=lesson-note--).+?(?=\W)/)?.[0] ?? "note"
+  );
+
+  headings[noteBoxType].push({
+    text: currentToken.line,
+    hashes: currentToken.markup,
+    lineNumber: currentToken.lineNumber,
+  });
+  return headings;
+}
+
 module.exports = {
   names: ["TOP011", "note-box-headings"],
   description: "Note boxes have appropriate headings",
@@ -7,29 +46,55 @@ module.exports = {
   tags: ["headings"],
   parser: "markdownit",
   function: function TOP011(params, onError) {
-    const noteBoxHeadings = params.parsers.markdownit.tokens.filter((token, index, tokens) => {
-      const previousToken = tokens[index - 1];
-
-      const isFollowingNoteBoxOpening =
-        previousToken?.line?.startsWith("<div ") && previousToken.line.includes("lesson-note");
-      return token.type === "heading_open" && isFollowingNoteBoxOpening;
+    const { tokens } = params.parsers.markdownit;
+    // console.log(tokens);
+    const noteBoxesWithoutHeadings = tokens.filter(lacksHeading);
+    const noteBoxHeadings = tokens.reduce(categoriseHeadingsByNoteBoxType, {
+      Note: [],
+      Tip: [],
+      Warning: [],
+      Critical: [],
     });
 
-    noteBoxHeadings.forEach((heading) => {
-      const headingHashes = heading.markup;
-      if (headingHashes.length === 4) {
-        return;
+    noteBoxesWithoutHeadings.forEach((noteBox) => {
+      onError({
+        lineNumber: noteBox.lineNumber,
+        detail:
+          "Note box is missing a heading. Note boxes must start with a level 4 heading (####).",
+      });
+    });
+
+    Object.entries(noteBoxHeadings).forEach(([noteBoxType, headings]) => {
+      headings.forEach(({ text, hashes, lineNumber }) => {
+        if (text.trim().startsWith(`#### ${capitalize(noteBoxType)}:`)) {
+          return;
       }
 
-      const headingText = heading.line.slice(headingHashes.length + 1);
-      onError({
-        lineNumber: heading.lineNumber,
-        detail: `\n  Expected: #### ${headingText}\n  Actual: ${headingHashes} ${headingText}\n`,
-        fixInfo: {
-          editColumn: 1,
-          deleteCount: headingHashes.length,
-          insertText: "####",
-        },
+        const leadingSpaces = text.indexOf(hashes);
+        const firstWordIndex = leadingSpaces.length + hashes.length + 1;
+        const firstWord = text.slice(firstWordIndex, text.indexOf(":"));
+        const baseErrorInfo = {
+          lineNumber: lineNumber,
+          context: text,
+        };
+
+        if (firstWord !== noteBoxType) {
+          onError({
+            ...baseErrorInfo,
+            detail: `${noteBoxType} box heading text must start with "${noteBoxType}:"`,
+          });
+        }
+        if (hashes.length !== 4) {
+          onError({
+            ...baseErrorInfo,
+            detail: `Expected a level 4 heading (####) but got a level ${hashes.length} heading (${hashes}) instead.`,
+            fixInfo: {
+              editColumn: leadingSpaces + 1,
+              deleteCount: hashes.length,
+              insertText: "####",
+            },
+          });
+        }
       });
     });
   },
