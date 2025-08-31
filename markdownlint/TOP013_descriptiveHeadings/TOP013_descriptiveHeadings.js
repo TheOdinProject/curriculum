40099af77ceb23989066e25abc09a951f93845cf
mao-sz <122839503+mao-sz@users.noreply.github.com>
2025-08-31 23:04:50 +0100

    Write descriptive headings custom lint rule

---
+++ markdownlint/TOP013_descriptiveHeadings/TOP013_descriptiveHeadings.js
@@ -0,0 +1,47 @@
+const BLACKLISTED_HEADINGS = [
+  "note",
+  "notes",
+  "a note",
+  "tip",
+  "tips",
+  "a tip",
+  "warning",
+  "warnings",
+  "a warning",
+  "important",
+  "important note",
+  "important tip",
+  "important warning",
+  "info",
+  "information",
+  "critical",
+  "danger",
+  "remember",
+];
+
+module.exports = {
+  names: ["TOP013", "descriptive-headings"],
+  description: "Headings must have descriptive text",
+  information: new URL(
+    "https://github.com/TheOdinProject/curriculum/blob/main/markdownlint/docs/TOP013.md",
+  ),
+  tags: ["accessibility", "headings"],
+  parser: "markdownit",
+  function: function TOP012(params, onError) {
+    const { tokens } = params.parsers.markdownit;
+    const headings = tokens.filter((token) => token.type === "heading_open");
+
+    headings.forEach((heading) => {
+      const headingTextStartIndex = heading.markup.length + 1;
+      const headingText = heading.line.slice(headingTextStartIndex);
+
+      if (BLACKLISTED_HEADINGS.includes(headingText.toLowerCase())) {
+        onError({
+          lineNumber: heading.lineNumber,
+          detail: `"${headingText}" is not sufficiently descriptive by itself. Use a more descriptive heading that briefly but clearly summarises the content of the section.`,
+          context: heading.line,
+        });
+      }
+    });
+  },
+};
