804704f30129a9ddf33c3685a1369897c40eb96f
mao-sz <122839503+mao-sz@users.noreply.github.com>
2025-09-01 13:33:54 +0100

    Markdownlint: New descriptive headings rule (#30057)
    * Add TOP013 docs
    
    * Add new custom rule TOP013 to config
    
    * Add custom rule test file
    
    * Write descriptive headings custom lint rule
    
    * Account for trailing punctuation in headings
    
    For example, trailing exclamation marks are allowed by MD026 but
    `#### Remember!` or `#### Warning!`
    should still flag this lint rule. The trailing punctuation changes
    nothing.
    
    * Move accessible headings info to Headings section
    
    * Remove unnecessary loop
    
    * Use American English spelling in error message
    
    Consistent with other rules/repo style
    
    * Fix typo
    
    Co-authored-by: Manon <81025586+ManonLef@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Manon <81025586+ManonLef@users.noreply.github.com>

---
+++ markdownlint/TOP013_descriptiveHeadings/TOP013_descriptiveHeadings.js
@@ -0,0 +1,57 @@
+const BLACKLISTED_HEADINGS = [
+  "note",
+  "notes",
+  "a note",
+  "tip",
+  "tips",
+  "a tip",
+  "warning",
+  "warnings",
+  "a warning",
+  "important",
+  "important note",
+  "important tip",
+  "important warning",
+  "info",
+  "information",
+  "critical",
+  "danger",
+  "remember",
+];
+
+function removeTrailingPunctuation(text) {
+  // https://regexr.com/8grso to test this regex
+  return text.replaceAll(/\W+$/g, "");
+}
+
+module.exports = {
+  names: ["TOP013", "descriptive-headings"],
+  description: "Headings must have descriptive text",
+  information: new URL(
+    "https://github.com/TheOdinProject/curriculum/blob/main/markdownlint/docs/TOP013.md",
+  ),
+  tags: ["accessibility", "headings"],
+  parser: "markdownit",
+  function: function TOP013(params, onError) {
+    const { tokens } = params.parsers.markdownit;
+
+    tokens.forEach((token) => {
+      if (token.type !== "heading_open") {
+        return;
+      }
+
+      const headingTextStartIndex = token.markup.length + 1;
+      const headingText = removeTrailingPunctuation(
+        token.line.slice(headingTextStartIndex),
+      );
+
+      if (BLACKLISTED_HEADINGS.includes(headingText.toLowerCase())) {
+        onError({
+          lineNumber: token.lineNumber,
+          detail: `"${headingText}" is not sufficiently descriptive by itself. Use a more descriptive heading that briefly but clearly summarizes the content of the section.`,
+          context: token.line,
+        });
+      }
+    });
+  },
+};
