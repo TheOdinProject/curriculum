b3553bb28c55f3580c86af775328ffc42f04ae0b
mao-sz <122839503+mao-sz@users.noreply.github.com>
2025-12-30 00:06:00 +0000

    Add tests for TOP003 fixes
    Not all parts of TOP003 are fixable

--- markdownlint/TOP003_defaultSectionContent/tests/TOP003.test.js
+++ markdownlint/TOP003_defaultSectionContent/tests/TOP003.test.js
@@ -1,7 +1,9 @@
 const { join } = require("node:path");
+const { readFile } = require("node:fs/promises");
 const { describe, it } = require("node:test");
 const assert = require("node:assert/strict");
 const getLintErrors = require("../../test_utils/lint")(__dirname);
+const fixLintErrors = require("../../test_utils/fix")(__dirname);
 const rule = require("../TOP003_defaultSectionContent");
 
 const pathInRepo = "markdownlint/TOP003_defaultSectionContent/tests";
@@ -14,83 +16,116 @@ const expected = {
 };
 
 describe("TOP003", () => {
-  it("Links to the TOP003 docs", () => {
-    assert.deepEqual(rule.information, expected.information);
+  describe("Lint", () => {
+    it("Links to the TOP003 docs", () => {
+      assert.deepEqual(rule.information, expected.information);
+    });
+
+    it("Flags when extra content surrounds default section lists", async () => {
+      const filePath = "./content_around_list.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:9 error ${expected.name} ${expected.description} [Only an unordered list of lesson overviews can follow the default content.]`,
+        `${errorPath}:13 error ${expected.name} ${expected.description} [There should be no additional content after the unordered list of lesson overviews]`,
+      ]);
+    });
+
+    it("Flags when sections with default content are empty", async () => {
+      const filePath = "./empty_section.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:5 error ${expected.name} ${expected.description} [The lesson overview section cannot be empty]`,
+        `${errorPath}:19 error ${expected.name} ${expected.description} [The knowledge check section cannot be empty]`,
+        `${errorPath}:21 error ${expected.name} ${expected.description} [The additional resources section cannot be empty]`,
+      ]);
+    });
+
+    it("Flags when sections have incorrect default content", async () => {
+      const filePath = "./incorrect_content.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:7 error ${expected.name} ${expected.description} [Expected: "This section contains a general overview of topics that you will learn in this lesson."; Actual: "This section has the wrong text following the heading that should flag an error."]`,
+        `${errorPath}:25 error ${expected.name} ${expected.description} [Expect default content to precede unordered list of knowledge checks: "The following questions are an opportunity to reflect on key topics in this lesson. If you can't answer a question, click on it to review the material, but keep in mind you are not expected to memorize or master this knowledge."]`,
+      ]);
+    });
+
+    it("Flags when a section does not have a required unordered list", async () => {
+      const filePath = "./missing_list.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:7 error ${expected.name} ${expected.description} [Must include an unordered list of lesson overviews in the "lesson overview" section]`,
+        `${errorPath}:23 error ${expected.name} ${expected.description} [Must include an unordered list of knowledge checks in the "knowledge check" section]`,
+        `${errorPath}:27 error ${expected.name} ${expected.description} [Must include an unordered list of additional resources in the "additional resources" section]`,
+      ]);
+    });
+
+    it("Flags when the assignment section is missing the necessary div wrapper", async () => {
+      const filePath = "./missing_wrapper.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:15 error ${expected.name} ${expected.description} [Assignment sections must include an HTML div element with class="lesson-content__panel" and markdown="1" attributes]`,
+      ]);
+    });
+
+    it("Flags when ordered or nested lists are used for unordered list sections", async () => {
+      const filePath = "./nested_and_ordered_list.md";
+      const errorPath = join(pathInRepo, filePath);
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, [
+        `${errorPath}:10 error ${expected.name} ${expected.description} [The lesson overview section must not contain nested lists.]`,
+        `${errorPath}:29 error ${expected.name} ${expected.description} [The knowledge check section must not include any ordered lists.]`,
+        `${errorPath}:29 error ${expected.name} ${expected.description} [Must include an unordered list of knowledge checks in the "knowledge check" section]`,
+        `${errorPath}:30 error ${expected.name} ${expected.description} [The knowledge check section must not include any ordered lists.]`,
+      ]);
+    });
+
+    it("Does not flag any errors if no violations", async () => {
+      const filePath = "./valid.md";
+      const lintErrors = await getLintErrors(filePath);
+
+      assert.deepEqual(lintErrors, []);
+    });
   });
 
-  it("Flags when extra content surrounds default section lists", async () => {
-    const filePath = "./content_around_list.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
+  describe("Fix", () => {
+    it("Converts ordered lists to unordered lists", async () => {
+      const fixedFileContents = await fixLintErrors(
+        "./nested_and_ordered_list.md",
+      );
+      const correctFile = await readFile(
+        join(__dirname, "./fixed_nested_and_ordered_list.md"),
+      );
 
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:9 error ${expected.name} ${expected.description} [Only an unordered list of lesson overviews can follow the default content.]`,
-      `${errorPath}:13 error ${expected.name} ${expected.description} [There should be no additional content after the unordered list of lesson overviews]`,
-    ]);
-  });
+      assert.equal(fixedFileContents, correctFile.toString());
+    });
 
-  it("Flags when sections with default content are empty", async () => {
-    const filePath = "./empty_section.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
+    it("Inserts/replaces missing or incorrect default section content", async () => {
+      const fixedFileContents = await fixLintErrors("./incorrect_content.md");
+      const correctFile = await readFile(
+        join(__dirname, "./fixed_incorrect_content.md"),
+      );
 
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:5 error ${expected.name} ${expected.description} [The lesson overview section cannot be empty]`,
-      `${errorPath}:19 error ${expected.name} ${expected.description} [The knowledge check section cannot be empty]`,
-      `${errorPath}:21 error ${expected.name} ${expected.description} [The additional resources section cannot be empty]`,
-    ]);
-  });
+      assert.equal(fixedFileContents, correctFile.toString());
+    });
 
-  it("Flags when sections have incorrect default content", async () => {
-    const filePath = "./incorrect_content.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
+    it("Removes flagged content around default section lists", async () => {
+      const fixedFileContents = await fixLintErrors("./content_around_list.md");
+      const correctFile = await readFile(
+        join(__dirname, "./fixed_content_around_list.md"),
+      );
 
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:7 error ${expected.name} ${expected.description} [Expected: "This section contains a general overview of topics that you will learn in this lesson."; Actual: "This section has the wrong text following the heading that should flag an error."]`,
-      `${errorPath}:25 error ${expected.name} ${expected.description} [Expect default content to precede unordered list of knowledge checks: "The following questions are an opportunity to reflect on key topics in this lesson. If you can't answer a question, click on it to review the material, but keep in mind you are not expected to memorize or master this knowledge."]`,
-    ]);
-  });
-
-  it("Flags when a section does not have a required unordered list", async () => {
-    const filePath = "./missing_list.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
-
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:7 error ${expected.name} ${expected.description} [Must include an unordered list of lesson overviews in the "lesson overview" section]`,
-      `${errorPath}:23 error ${expected.name} ${expected.description} [Must include an unordered list of knowledge checks in the "knowledge check" section]`,
-      `${errorPath}:27 error ${expected.name} ${expected.description} [Must include an unordered list of additional resources in the "additional resources" section]`,
-    ]);
-  });
-
-  it("Flags when the assignment section is missing the necessary div wrapper", async () => {
-    const filePath = "./missing_wrapper.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
-
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:15 error ${expected.name} ${expected.description} [Assignment sections must include an HTML div element with class="lesson-content__panel" and markdown="1" attributes]`,
-    ]);
-  });
-
-  it("Flags when ordered or nested lists are used for unordered list sections", async () => {
-    const filePath = "./nested_and_ordered_list.md";
-    const errorPath = join(pathInRepo, filePath);
-    const lintErrors = await getLintErrors(filePath);
-
-    assert.deepEqual(lintErrors, [
-      `${errorPath}:10 error ${expected.name} ${expected.description} [The lesson overview section must not contain nested lists.]`,
-      `${errorPath}:29 error ${expected.name} ${expected.description} [The knowledge check section must not include any ordered lists.]`,
-      `${errorPath}:29 error ${expected.name} ${expected.description} [Must include an unordered list of knowledge checks in the "knowledge check" section]`,
-      `${errorPath}:30 error ${expected.name} ${expected.description} [The knowledge check section must not include any ordered lists.]`,
-    ]);
-  });
-
-  it("Does not flag any errors if no violations", async () => {
-    const filePath = "./valid.md";
-    const lintErrors = await getLintErrors(filePath);
-
-    assert.deepEqual(lintErrors, []);
+      assert.equal(fixedFileContents, correctFile.toString());
+    });
   });
 });
